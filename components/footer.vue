96b89692cbb0cf593ce998f15bc88872277d0f53
Ridhima10 <ridhimajain862@gmail.com>
2022-05-28 21:48:15 +0530

    added footer prototype

---
+++ components/footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- Footer -->
+<footer class="bg-light text-center ">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <section class="mb-4">
+      <p>Logo</p>
+      <p>UPASTHITI</p>
+    </section>
+    <section class="">
+      <!--Grid row-->
+      <div class=" text-center  list-unstyled">
+              <nuxt-link to="/" class="text-dark px-2">Link 1</nuxt-link>
+              <nuxt-link to="/" class="text-dark px-2">Link 2</nuxt-link>
+              <nuxt-link to="/"  class="text-dark px-2">Link 3</nuxt-link>
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3 bg-dark text-light">
+    Copyrights and year etc
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
