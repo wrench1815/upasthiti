96440a516e5d9fef6c73a324c44d1063b08a2b8f
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-05-30 18:23:38 +0530

    refactor: rename Footer Component, update Footer Code

---
+++ components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- start:Footer -->
+  <footer class="bg-white text-center">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <section class="mb-4">
+        <i>Logo</i>
+        <h3 class="fw-bold text-primary">Upasthiti</h3>
+      </section>
+      <section class="">
+        <!--Grid row-->
+        <div class="d-flex justify-content-center align-items-center flex-wrap">
+          <nuxt-link to="/" class="px-2 text-dark">Home</nuxt-link>
+          <nuxt-link to="/about" class="px-2 text-dark">About</nuxt-link>
+          <nuxt-link to="/" class="px-2 text-dark">Link 3</nuxt-link>
+          </div>
+        <!--Grid row-->
+      </section>
+      <!-- Section: Links -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- start:Copyright -->
+    <div class="d-flex justify-content-center align-items-center pb-3">
+      <i class="ri-copyright-line"></i>Upasthiti {{ currentYear }}
+    </div>
+    <!-- end:Copyright -->
+  </footer>
+  <!-- end:Footer -->
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    }
+  },
+}
+</script>
+
+<style></style>
