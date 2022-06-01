f948a295701236e78a78f1f3475caa8b4361d431
Ridhima10 <ridhimajain862@gmail.com>
2022-06-01 22:51:55 +0530

    Added Department fields

---
+++ components/DepartmentField.vue
@@ -0,0 +1,194 @@
+<template>
+  <section class="container">
+    <form action="" class="card card-body">
+      <!-- start:Department  Name -->
+      <div class="row my-4">
+        <div class="col-6">
+          <label class="form-label fw-bold"
+            >Department Name
+            <i class="ri-asterisk text-danger ri-sm"></i>
+          </label>
+        </div>
+        <div class="col-6">
+          <div class="form-outline">
+            <input type="text" class="form-control" />
+          </div>
+        </div>
+      </div>
+      <!-- end:Department Name -->
+
+      <!-- start:Department code -->
+      <div class="row my-4">
+        <div class="col-6">
+          <label class="form-label fw-bold"
+            >Department Code
+            <i class="ri-asterisk ri-sm text-danger"></i>
+          </label>
+        </div>
+        <div class="col-6">
+          <div class="form-outline">
+            <input type="text" class="form-control" />
+          </div>
+        </div>
+      </div>
+      <!-- end:Department code -->
+
+      <!-- start:Types of programmes offered-->
+      <div class="row my-4">
+        <div class="col-6">
+          <label class="form-label fw-bold"
+            >Types Of Programmes Offered
+            <i class="ri-asterisk ri-sm text-danger"></i>
+          </label>
+        </div>
+        <div class="col-6">
+          <v-select
+            placeholder="Choose.."
+            :options="availTypesOfProgrammes"
+          ></v-select>
+        </div>
+      </div>
+      <!--end:Types of programmes offered-->
+
+      <!-- start:Total Semesters-->
+      <div class="row my-4">
+        <div class="col-6">
+          <label class="form-label fw-bold"
+            >Total Semesters
+            <i class="ri-asterisk ri-sm text-danger"></i>
+          </label>
+        </div>
+        <div class="col-6">
+          <v-select placeholder="Choose.." :options="availSemesters"></v-select>
+        </div>
+      </div>
+      <!--end:Total semesters-->
+
+      <!--start: Student Intake -->
+      <div class="row my-4">
+        <div class="col-6">
+          <label class="form-label fw-bold"
+            >Student Intake <i class="ri-asterisk ri-sm text-danger"></i>
+          </label>
+        </div>
+        <div class="col-6">
+          <div class="form-outline">
+            <input type="number" class="form-control" />
+          </div>
+        </div>
+      </div>
+      <!--end:Student Intake -->
+
+      <!--start: Faculty Intake -->
+      <div class="row my-4">
+        <div class="col-6">
+          <label class="form-label fw-bold"
+            >Faculty Intake
+            <i class="ri-asterisk ri-sm text-danger"></i>
+          </label>
+        </div>
+        <div class="col-6">
+          <div class="form-outline">
+            <input type="number" class="form-control" />
+          </div>
+        </div>
+      </div>
+      <!--end:Faculty Intake-->
+
+      <!--start: No. Of Professors -->
+      <div class="row my-4">
+        <div class="col-6">
+          <label class="form-label fw-bold"
+            >No. Of Professors
+            <i class="ri-asterisk ri-sm text-danger"></i>
+          </label>
+        </div>
+        <div class="col-6">
+          <div class="form-outline">
+            <input type="number" class="form-control" />
+          </div>
+        </div>
+      </div>
+      <!--end:No. Of Professors-->
+
+      <!--start: No. Of Associate Professors -->
+      <div class="row my-4">
+        <div class="col-6">
+          <label class="form-label fw-bold">
+            No. Of Associate Professors
+            <i class="ri-asterisk ri-sm text-danger"></i>
+          </label>
+        </div>
+        <div class="col-6">
+          <div class="form-outline">
+            <input type="number" class="form-control" />
+          </div>
+        </div>
+      </div>
+      <!--end: No. Of Associate Professors-->
+
+      <!--start: No. Of Assistant Professors -->
+      <div class="row my-4">
+        <div class="col-6">
+          <label class="form-label fw-bold"
+            >No. Of Assistant Professors
+            <i class="ri-asterisk ri-sm text-danger"></i>
+          </label>
+        </div>
+        <div class="col-6">
+          <div class="form-outline">
+            <input type="number" class="form-control" />
+          </div>
+        </div>
+      </div>
+      <!--end:No. Of Assistant Professors-->
+
+      <!--start: HOD-->
+      <div class="row my-4">
+        <div class="col-6">
+          <label class="form-label fw-bold"
+            >HOD
+            <i class="ri-asterisk ri-sm text-danger"></i>
+          </label>
+        </div>
+        <div class="col-6">
+          <div class="form-outline">
+            <input type="number" class="form-control" />
+          </div>
+        </div>
+      </div>
+      <!--end:HOD-->
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'DepartmentField',
+
+  data() {
+    return {
+      Types: '',
+      availTypesOfProgrammes: [
+        'Under-Graduate',
+        'Post-Graduate',
+        'Integrated PG',
+        'M.Phil',
+        'Ph.D',
+        'Certificate',
+        'UG Diploma',
+        'PG Diploma',
+      ],
+      availSemesters: [1, 2, 3, 4, 5, 6, 7, 8],
+    }
+  },
+
+  mounted() {
+    document.querySelectorAll('.form-outline').forEach((formOutline) => {
+      new this.$mdb.Input(formOutline).init()
+    })
+  },
+}
+</script>
+
+<style></style>
