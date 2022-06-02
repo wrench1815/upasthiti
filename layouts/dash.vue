e66327e2f1a64715a3dcc048f7a58f347f024ec3
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-06-02 16:55:30 +0530

    feat: add base layout for dash

---
+++ layouts/dash.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Lazy-DashNavBar />
+    <Lazy-DashSideBar />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashLayout',
+}
+</script>
+
+<style></style>
