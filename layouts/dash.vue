e05d080f0e05bb2d73cb118065c4fd19f79b04fb
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-06-04 01:21:09 +0530

    feat:add componets for Dash
    add header bg
    add Toolbar
    add to top util
    add footer

--- layouts/dash.vue
+++ layouts/dash.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
+  <div class="bg-nav">
     <Lazy-DashNavBar />
+    <Lazy-DashToolBar />
     <Lazy-DashSideBar />
+
     <nuxt />
+    <Lazy-UtilsToTop />
+    <Lazy-DashFooter />
   </div>
 </template>
 
@@ -12,4 +16,12 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+.bg-nav {
+  background-image: url(~assets/images/curved.jpg);
+  background-size: 100% 50vh;
+  background-color: #f5f8fa;
+  background-repeat: no-repeat;
+  background-position: center top;
+}
+</style>
