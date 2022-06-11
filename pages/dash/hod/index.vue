90bcb8d1219d23503148082f82eb8c4ee2b9cc3d
Ridhima10 <ridhimajain862@gmail.com>
2022-06-11 14:44:46 +0530

    Added all dash root pages

---
+++ pages/dash/hod/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container my-4">
+    <div class="card">
+      <div class="card-body">HOD Index</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashHodIndex',
+  layout: 'dash',
+
+  created() {
+    this.$store.commit('breadCrumbs/addBreadCrumb', [
+      {
+        name: 'Hod',
+        url: '/hod',
+      },
+    ])
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/dash/student/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container my-4">
+    <div class="card">
+      <div class="card-body">Student Index</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashStudentIndex',
+  layout: 'dash',
+
+  created() {
+    this.$store.commit('breadCrumbs/addBreadCrumb', [
+      {
+        name: 'Student',
+        url: '/student',
+      },
+    ])
+  },
+}
+</script>
+
+<style></style>
