0db6c9890167de8f35f6a952b4311b3a6d5f1f82
Ridhima10 <ridhimajain862@gmail.com>
2022-06-11 13:59:43 +0530

    feat: remove college single page and add college dash page logic using folder

---
+++ pages/dash/college/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container my-4">
+    <div class="card">
+      <div class="card-body">College Index</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashCollegeIndex',
+  layout: 'dash',
+
+  created() {
+    this.$store.commit('breadCrumbs/addBreadCrumb', [
+      {
+        name: 'College',
+        url: '/college',
+      },
+    ])
+  },
+}
+</script>
+
+<style></style>
