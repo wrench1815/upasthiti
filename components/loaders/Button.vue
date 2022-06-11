4c74168f71dc79ffda8926bbf2232ade10fd7e09
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-06-12 03:54:09 +0530

    feat: add loaders for table and button

---
+++ components/loaders/Button.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="btn placeholder placeholder-wave col-4 col-md-2 col-lg-1"
+    :class="classes"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: 'LoadersButton',
+
+  props: {
+    btnColor: {
+      type: String,
+      default: 'btn-primary',
+    },
+
+    rounded: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      classes: '',
+    }
+  },
+
+  created() {
+    this.classes = `${this.btnColor}`
+    if (this.rounded) {
+      this.classes += ' btn-rounded'
+    }
+  },
+}
+</script>
+
+<style></style>
---
+++ components/loaders/Table.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="table-responsive">
+    <table class="table table-hover table-bordered align-middle">
+      <thead class="align-middle">
+        <tr>
+          <th scope="col" class="placeholder-wave">
+            <span class="placeholder col-12 placeholder-width"></span>
+          </th>
+          <th scope="col" class="placeholder-wave">
+            <span class="placeholder col-12 placeholder-width"></span>
+          </th>
+          <th scope="col" class="placeholder-wave">
+            <span class="placeholder col-12 placeholder-width"></span>
+          </th>
+          <th scope="col" class="placeholder-wave">
+            <span class="placeholder col-12 placeholder-width"></span>
+          </th>
+
+          <th scope="col" class="placeholder-wave">
+            <span class="placeholder col-12 placeholder-width"></span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in 5">
+          <td class="placeholder-wave">
+            <span class="placeholder col-12"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-8"></span>
+          </td>
+          <td class="placeholder-wave">
+            <span class="placeholder col-12"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-8"></span>
+          </td>
+          <td class="placeholder-wave">
+            <span class="placeholder col-12"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-8"></span>
+          </td>
+          <td class="placeholder-wave">
+            <span class="placeholder col-12"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-8"></span>
+          </td>
+          <td class="placeholder-wave">
+            <span
+              class="btn btn-floating btn-info btn-sm placeholder col-1"
+            ></span>
+            <span
+              class="btn btn-floating btn-danger btn-sm placeholder col-1"
+            ></span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadersTable',
+}
+</script>
+
+<style scoped>
+.placeholder-width {
+  width: 5rem;
+}
+</style>
