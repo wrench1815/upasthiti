bfe0a354f857eb8e336b38dc819658d1eec1fcb4
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-06-12 18:40:22 +0530

    feat: add ability to delete user

--- components/Dash/UserTable.vue
+++ components/Dash/UserTable.vue
@@ -20,20 +20,15 @@
           </td>
           <td class="d-flex gap-2">
             <NuxtLink
-              :to="`/dash/user/detail?id=${user.id}`"
-              class="btn btn-floating btn-success btn-sm d-flex justify-content-center align-items-center"
-              ><i class="ri-eye-fill ri-lg"></i
-            ></NuxtLink>
-            <NuxtLink
               :to="`/dash/user/${user.id}`"
               class="btn btn-floating btn-info btn-sm d-flex justify-content-center align-items-center"
               ><i class="ri-edit-2-fill ri-lg"></i
             ></NuxtLink>
-            <NuxtLink
-              :to="`/dash/user/${user.id}`"
+            <a
+              @click="deleteUser(user.id)"
               class="btn btn-floating btn-danger btn-sm d-flex justify-content-center align-items-center"
               ><i class="ri-delete-bin-fill ri-lg"></i
-            ></NuxtLink>
+            ></a>
           </td>
         </tr>
       </tbody>
@@ -56,6 +51,55 @@ export default {
     dateFormat(date) {
       // return strf date
       return this.$moment(date).format('Do MMMM YYYY, h:mm:ss a')
+    },
+
+    deleteUser(id) {
+      this.$swal({
+        title: 'Are you sure?',
+        text: 'You will not be able to recover this user!',
+        icon: 'warning',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, keep it',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.$swal({
+            title: 'Processing...',
+            text: 'Please wait a moment while we are deleting the user',
+            icon: 'info',
+            type: 'info',
+            didOpen: () => {
+              this.$swal.showLoading()
+
+              this.$api.user.destroy(id).then(() => {
+                this.$swal.hideLoading()
+                this.$swal.close()
+
+                this.$swal({
+                  title: 'Deleted!',
+                  text: 'User has been deleted.',
+                  icon: 'success',
+                  type: 'success',
+                })
+                  .then(() => {
+                    this.users = this.users.filter((user) => user.id !== id)
+                  })
+                  .catch((err) => {
+                    this.$swal.close()
+
+                    this.$swal({
+                      title: 'Error',
+                      text: err.response.data.detail,
+                      icon: 'error',
+                      type: 'error',
+                    })
+                  })
+              })
+            },
+          })
+        }
+      })
     },
   },
 }
