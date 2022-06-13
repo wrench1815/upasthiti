9a8e4a604b8b22328e3654aeb313d4c6e7f55ca1
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-06-13 14:19:27 +0530

    add dummy data for detail page

--- pages/dash/college/detail.vue
+++ pages/dash/college/detail.vue
@@ -1,7 +1,204 @@
-<template></template>
+<template>
+  <div class="container my-4">
+    <div class="card">
+      <div class="card-header">
+        <h2>College Details</h2>
+        <p>College Details</p>
+      </div>
+      <div class="card-body">
+        <div class="row mb-3">
+          <div class="col-12 col-md-4">
+            <label class="text-muted fw-bold-500">Institute name</label>
+          </div>
+          <div class="col">
+            <span class="fw-bold-600 fs-6">test dta</span>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-12 col-md-4">
+            <label class="text-muted fw-bold-500">Institute name</label>
+          </div>
+          <div class="col">
+            <span class="fw-bold-600 fs-6">test dta</span>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-12 col-md-4">
+            <label class="text-muted fw-bold-500">Institute name</label>
+          </div>
+          <div class="col">
+            <span class="fw-bold-600 fs-6">test dta</span>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-12 col-md-4">
+            <label class="text-muted fw-bold-500">Institute name</label>
+          </div>
+          <div class="col">
+            <span class="fw-bold-600 fs-6">test dta</span>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-12 col-md-4">
+            <label class="text-muted fw-bold-500">Institute name</label>
+          </div>
+          <div class="col">
+            <span class="fw-bold-600 fs-6">test dta</span>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-12 col-md-4">
+            <label class="text-muted fw-bold-500">Institute name</label>
+          </div>
+          <div class="col">
+            <span class="fw-bold-600 fs-6">test dta</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card mt-4">
+      <div class="card-header">
+        <h2>College Principal Details</h2>
+        <p>Details of College Principal</p>
+      </div>
+      <div class="card-body">
+        <div class="row mb-3">
+          <div class="col-12 col-md-4">
+            <label class="text-muted fw-bold-500">Institute name</label>
+          </div>
+          <div class="col">
+            <span class="fw-bold-600 fs-6">test dta</span>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-12 col-md-4">
+            <label class="text-muted fw-bold-500">Institute name</label>
+          </div>
+          <div class="col">
+            <span class="fw-bold-600 fs-6">test dta</span>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-12 col-md-4">
+            <label class="text-muted fw-bold-500">Institute name</label>
+          </div>
+          <div class="col">
+            <span class="fw-bold-600 fs-6">test dta</span>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-12 col-md-4">
+            <label class="text-muted fw-bold-500">Institute name</label>
+          </div>
+          <div class="col">
+            <span class="fw-bold-600 fs-6">test dta</span>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-12 col-md-4">
+            <label class="text-muted fw-bold-500">Institute name</label>
+          </div>
+          <div class="col">
+            <span class="fw-bold-600 fs-6">test dta</span>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-12 col-md-4">
+            <label class="text-muted fw-bold-500">Institute name</label>
+          </div>
+          <div class="col">
+            <span class="fw-bold-600 fs-6">test dta</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card mt-4">
+      <div class="card-header">
+        <h2>Departmenft Details</h2>
+        <p>Details of College Department</p>
+      </div>
+      <div class="card-body">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+          <div class="col">
+            <div class="card">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp"
+                class="card-img-top"
+                alt="Hollywood Sign on The Hill"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp"
+                class="card-img-top"
+                alt="Palm Springs Road"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp"
+                class="card-img-top"
+                alt="Los Angeles Skyscrapers"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp"
+                class="card-img-top"
+                alt="Skyscrapers"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {}
+export default {
+  name: 'DashCollegeDetail',
+  layout: 'dash',
+}
 </script>
 
 <style></style>
