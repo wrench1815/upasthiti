35e9552875bffd57fa5d57caba7b3c463619d474
Ridhima10 <ridhimajain862@gmail.com>
2022-06-13 13:47:48 +0530

    Added college dash page

---
+++ pages/dash/college/add.vue
@@ -0,0 +1,411 @@
+<template>
+  <div class="container my-4">
+    <div class="card">
+      <div class="card-header">
+        <h2>Add College</h2>
+        <p>Add a New College</p>
+      </div>
+      <div class="card-body">
+        <!-- for Valdation -->
+        <ValidationObserver v-slot="{ handleSubmit }">
+          <!-- start:User Edit Form -->
+          <form @submit.prevent="handleSubmit(addUser)">
+            <!-- start:Institute name-->
+            <div class="row">
+              <div class="col-12 col-md-4">
+                <label class="form-label" for="institute_name"
+                  >Institute name</label
+                >
+              </div>
+              <div class="col">
+                <ValidationProvider
+                  v-slot="{ errors }"
+                  :rules="{ required: true, min: 3, alpha: true }"
+                >
+                  <div class="form-outline">
+                    <input
+                      type="text"
+                      id="institute_name"
+                      class="form-control"
+                      v-model="user.first_name"
+                    />
+                  </div>
+                  <!-- Validation Errors -->
+                  <div
+                    class="text-danger"
+                    :class="{ 'mb-4': !errors[0], 'mb-2': errors[0] }"
+                  >
+                    {{ errors[0] }}
+                  </div>
+                </ValidationProvider>
+              </div>
+            </div>
+            <!-- end:Institute name -->
+
+            <!-- start:Institute Alias name -->
+            <div class="row">
+              <div class="col-12 col-md-4">
+                <label class="form-label" for="institute_alias_name"
+                  >Institute Alias</label
+                >
+              </div>
+              <div class="col">
+                <ValidationProvider
+                  v-slot="{ errors }"
+                  :rules="{ required: true, min: 3, alpha: true }"
+                >
+                  <div class="form-outline">
+                    <input
+                      type="text"
+                      id="institute_alias_name"
+                      class="form-control"
+                      v-model="user.last_name"
+                    />
+                  </div>
+                  <!-- Validation Errors -->
+                  <div
+                    class="text-danger"
+                    :class="{ 'mb-4': !errors[0], 'mb-2': errors[0] }"
+                  >
+                    {{ errors[0] }}
+                  </div>
+                </ValidationProvider>
+              </div>
+            </div>
+            <!-- end:Institute Alias name -->
+
+            <!-- start:Address-->
+            <div class="row">
+              <div class="col-12 col-md-4">
+                <label class="form-label" for="institute_address"
+                  >Address</label
+                >
+              </div>
+              <div class="col">
+                <ValidationProvider
+                  v-slot="{ errors }"
+                  :rules="{ required: true, email: true }"
+                >
+                  <div class="form-outline">
+                    <input
+                      type="text"
+                      id="institute_address"
+                      class="form-control"
+                      v-model="user.email"
+                    />
+                  </div>
+                  <!-- Validation Errors -->
+                  <div
+                    class="text-danger"
+                    :class="{ 'mb-4': !errors[0], 'mb-2': errors[0] }"
+                  >
+                    {{ errors[0] }}
+                  </div>
+                </ValidationProvider>
+              </div>
+            </div>
+            <!-- end:Address-->
+
+            <!-- start:Principal -->
+            <div class="row">
+              <div class="col-12 col-md-4">
+                <label class="form-label" for="institute_principal"
+                  >Principal</label
+                >
+              </div>
+              <div class="col">
+                <ValidationProvider
+                  v-slot="{ errors }"
+                  :rules="{ required: true }"
+                >
+                  <v-select
+                    placeholder="Select Gender"
+                    :options="genderList"
+                    v-model="user.gender"
+                  ></v-select>
+                  <!-- Validation Errors -->
+                  <div
+                    class="text-danger"
+                    :class="{ 'mb-4': !errors[0], 'mb-2': errors[0] }"
+                  >
+                    {{ errors[0] }}
+                  </div>
+                </ValidationProvider>
+              </div>
+            </div>
+            <!-- end:Principal-->
+
+            <!-- start:Logo-->
+            <div class="row">
+              <div class="col-12 col-md-4">
+                <label class="form-label" for="institute_logo">Logo</label>
+              </div>
+              <div class="col">
+                <ValidationProvider
+                  v-slot="{ errors }"
+                  :rules="{
+                    required: true,
+                    min: 6,
+                    max: 16,
+                    passwordNumber: true,
+                    passwordUpper: true,
+                    passwordLower: true,
+                    passwordSpecial: true,
+                  }"
+                >
+                  <div class="form-outline">
+                    <input
+                      type="text"
+                      id="institute_logo"
+                      name="password"
+                      class="form-control"
+                      v-model="user.password"
+                    />
+                  </div>
+                  <!-- Validation Errors -->
+                  <div
+                    class="text-danger"
+                    :class="{ 'mb-2': !errors[0], 'mb-1': errors[0] }"
+                  >
+                    {{ errors[0] }}
+                  </div>
+                </ValidationProvider>
+                <!-- start:Show password-->
+                <div class="form-check mb-4">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    id="showPassword"
+                    v-model="showPassword"
+                  />
+                  <label class="form-check-label" for="showPassword"
+                    >Show Password</label
+                  >
+                </div>
+                <!-- start:Show Password -->
+              </div>
+            </div>
+            <!-- end:Logo-->
+
+            <!-- start:Website-->
+            <div class="row">
+              <div class="col-12 col-md-4">
+                <label class="form-label" for="institute_website"
+                  >Website</label
+                >
+              </div>
+              <div class="col">
+                <ValidationProvider
+                  v-slot="{ errors }"
+                  :rules="{ required: true, min: 3, alpha: true }"
+                >
+                  <div class="form-outline">
+                    <input
+                      type="text"
+                      id="institute_website"
+                      class="form-control"
+                      v-model="user.first_name"
+                    />
+                  </div>
+                  <!-- Validation Errors -->
+                  <div
+                    class="text-danger"
+                    :class="{ 'mb-4': !errors[0], 'mb-2': errors[0] }"
+                  >
+                    {{ errors[0] }}
+                  </div>
+                </ValidationProvider>
+              </div>
+            </div>
+            <!-- end:website -->
+
+            <!-- start:Mobile-->
+            <div class="row">
+              <div class="col-12 col-md-4">
+                <label class="form-label" for="institute_mobile">Mobile</label>
+              </div>
+              <div class="col">
+                <ValidationProvider
+                  v-slot="{ errors }"
+                  :rules="{ required: true, min: 3, alpha: true }"
+                >
+                  <div class="form-outline">
+                    <input
+                      type="number"
+                      id="institute_mobile"
+                      class="form-control"
+                      v-model="user.first_name"
+                    />
+                  </div>
+                  <!-- Validation Errors -->
+                  <div
+                    class="text-danger"
+                    :class="{ 'mb-4': !errors[0], 'mb-2': errors[0] }"
+                  >
+                    {{ errors[0] }}
+                  </div>
+                </ValidationProvider>
+              </div>
+            </div>
+            <!-- end:Mobile -->
+
+            <!-- start:Email-->
+            <div class="row">
+              <div class="col-12 col-md-4">
+                <label class="form-label" for="institute_email">Email</label>
+              </div>
+              <div class="col">
+                <ValidationProvider
+                  v-slot="{ errors }"
+                  :rules="{ required: true, min: 3, alpha: true }"
+                >
+                  <div class="form-outline">
+                    <input
+                      type="email"
+                      id="institute_email"
+                      class="form-control"
+                      v-model="user.first_name"
+                    />
+                  </div>
+                  <!-- Validation Errors -->
+                  <div
+                    class="text-danger"
+                    :class="{ 'mb-4': !errors[0], 'mb-2': errors[0] }"
+                  >
+                    {{ errors[0] }}
+                  </div>
+                </ValidationProvider>
+              </div>
+            </div>
+            <!-- end:Email-->
+
+            <!-- Submit button -->
+            <div class="d-flex justify-content-end">
+              <button type="submit" class="btn btn-info btn-rounded mb-4">
+                Add College
+              </button>
+            </div>
+          </form>
+          <!-- End:User Edit Form -->
+        </ValidationObserver>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashUserAdd',
+  layout: 'dash',
+
+  data() {
+    return {
+      loading: true,
+      error: true,
+      genderList: ['Male', 'Female', 'Rather not Say'],
+      showPassword: false,
+      user: {
+        first_name: '',
+        last_name: '',
+        email: '',
+        gender: '',
+        is_admin: false,
+        is_principal: false,
+        is_hod: false,
+        is_teacher: false,
+        password: '',
+      },
+    }
+  },
+
+  created() {
+    this.$store.commit('breadCrumbs/addBreadCrumb', [
+      {
+        name: 'College',
+        url: '/dash/college',
+      },
+      {
+        name: 'Add',
+      },
+    ])
+  },
+
+  methods: {
+    async addUser() {
+      try {
+        const user = {
+          first_name: this.user.first_name,
+          last_name: this.user.last_name,
+          email: this.user.email,
+          gender: this.user.gender,
+          password: this.user.password,
+          confirm_password: this.user.password,
+          is_active: this.user.is_active,
+          is_admin: this.user.is_admin,
+          is_principal: this.user.is_principal,
+          is_hod: this.user.is_hod,
+          is_teacher: this.user.is_teacher,
+        }
+
+        this.$swal({
+          title: 'Adding User',
+          icon: 'info',
+          type: 'info',
+          text: 'Please wait while we are Adding a New User',
+          didOpen: () => {
+            this.$swal.showLoading()
+
+            const response = this.$api.user
+              .create(user)
+              .then(() => {
+                this.$swal.hideLoading()
+                this.$swal.close()
+
+                let timerInterval
+
+                this.$swal({
+                  title: 'Success',
+                  icon: 'success',
+                  type: 'success',
+                  text: 'User has been added Successfully',
+                  timer: 2000,
+                  timerProgressBar: true,
+
+                  didOpen: () => {
+                    this.$swal.showLoading()
+                  },
+                }).then(() => this.$router.push('/dash/user'))
+              })
+              .catch((err) => {
+                this.$swal.hideLoading()
+                this.$swal.close()
+
+                this.$swal({
+                  title: 'Error',
+                  icon: 'error',
+                  type: 'error',
+                  html: `Failed to Add User.`,
+                })
+              })
+          },
+        })
+      } catch (e) {
+        this.$swal({
+          title: 'Error',
+          icon: 'error',
+          type: 'error',
+          html: `Failed to Add User.<br/>Try Again`,
+        })
+      }
+    },
+  },
+
+  mounted() {
+    document.querySelectorAll('.form-outline').forEach((formOutline) => {
+      new this.$mdb.Input(formOutline).init()
+    })
+  },
+}
+</script>
+
+<style></style>
--- pages/dash/college/index.vue
+++ pages/dash/college/index.vue
@@ -1,7 +1,36 @@
 <template>
   <div class="container my-4">
     <div class="card">
-      <div class="card-body">College Index</div>
+      <div class="card-header">
+        <h2>Colleges</h2>
+        <p>lists all Registered Colleges</p>
+      </div>
+      <div class="card-body">
+        <section class="d-flex justify-content-end">
+          <div class="w-10-rem mb-3">
+            <v-select
+              placeholder="Select Filter"
+              :options="userFilters"
+              v-model="selectedUserFilter"
+              :clearable="false"
+              @option:selected="refreshUsers"
+            >
+            </v-select>
+          </div>
+        </section>
+        <Lazy-LoadersTable v-if="loading" />
+        <Lazy-DashUserTable v-if="!loading" :users="users" />
+
+        <div class="d-flex justify-content-end">
+          <Lazy-LoadersButton v-if="loading" :rounded="true" />
+          <Lazy-UtilsLinkButton
+            v-if="!loading"
+            :rounded="true"
+            :link="'/dash/user/add'"
+            >Add new College</Lazy-UtilsLinkButton
+          >
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,15 +40,90 @@ export default {
   name: 'DashCollegeIndex',
   layout: 'dash',
 
+  data() {
+    return {
+      users: [],
+      loading: true,
+      error: true,
+      userFilters: ['All', 'Admin', 'Principal', 'HOD', 'Teacher'],
+      selectedUserFilter: 'All',
+    }
+  },
+
   created() {
     this.$store.commit('breadCrumbs/addBreadCrumb', [
       {
         name: 'College',
-        url: '/college',
+        url: '/dash/college',
       },
     ])
+  },
+
+  methods: {
+    async getUsers() {
+      this.loading = true
+      let requestUrl
+
+      if (this.selectedUserFilter === 'All') {
+        requestUrl = this.$api.user.list()
+      } else if (this.selectedUserFilter === 'Admin') {
+        requestUrl = this.$api.user.listAdmin()
+      } else if (this.selectedUserFilter === 'Principal') {
+        requestUrl = this.$api.user.listPrincipal()
+      } else if (this.selectedUserFilter === 'HOD') {
+        requestUrl = this.$api.user.listHod()
+      } else if (this.selectedUserFilter === 'Teacher') {
+        requestUrl = this.$api.user.listTeacher()
+      }
+
+      const response = await requestUrl
+        .then(
+          (response) => ((this.users = response.data), (this.error = false))
+        )
+        .catch((error) => {
+          console.log(error)
+          this.$swal({
+            title: 'Error',
+            icon: 'error',
+            type: 'error',
+            html: `${
+              error.response.data.detail
+                ? error.response.data.detail
+                : 'Something went wrong'
+            }`,
+            confirmButtonText: 'Refresh',
+            showCancelButton: true,
+            cancelButtonText: 'To Dash Home',
+            confirmButtonClass: 'btn btn-info',
+          }).then((result) => {
+            if (result.isConfirmed) {
+              this.getUsers()
+            } else if (result.isDismissed) {
+              this.$router.push('/dash')
+            }
+          })
+        })
+    },
+
+    refreshUsers() {
+      this.loading = true
+      this.getUsers().then(() => (this.loading = false))
+    },
+
+    dateFormat(date) {
+      // return strf date
+      return this.$moment(date).format('Do MMMM YYYY, h:mm:ss a')
+    },
+  },
+
+  mounted() {
+    this.getUsers().finally(() => {
+      if (!this.error) {
+        this.loading = false
+      }
+    })
   },
 }
 </script>
 
-<style></style>
+<style scoped></style>
