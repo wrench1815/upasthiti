31a0bda8c904c7f9dff4990fc9b68d1fd8ff4b82
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-06-14 20:52:40 +0530

    feat: register college api endpoints globally

--- plugins/api.js
+++ plugins/api.js
@@ -1,9 +1,11 @@
 import User from '@/api/user'
+import College from '@/api/college'
 
 export default (context, inject) => {
   // Initialize API factories
   const factories = {
     user: User(context.$axios),
+    college: College(context.$axios),
   }
 
   // Inject $api
