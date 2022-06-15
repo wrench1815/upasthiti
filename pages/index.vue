ace38ca0f77948efabc4dab94b4bea1f465e919f
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-06-15 16:50:46 +0530

    fix: patched issue with out of bound index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header class="container my-4 car">
+    <header class="container my-4">
       <div class="row">
         <div
           class="col-md-6 col-lg-6 col-12 d-flex justify-content-center align-items-center"
@@ -27,7 +27,7 @@
         </div>
       </div>
     </header>
-    <section class="container-flui my-4 bg-white rounded-4 shadow-4">
+    <section class="my-4 bg-white rounded-4 shadow-4">
       <div class="container my-4 py-5">
         <div class="d-flex justify-content-center">
           <div class="row">
@@ -72,43 +72,18 @@
           </div>
         </div>
       </div>
-      <div class="row g-5">
-        <div class="col-4 mb-5">
+      <div class="row">
+        <div
+          class="col-12 col-md-4 col-lg-3 mb-5"
+          v-for="item in 4"
+          :key="item"
+        >
           <div class="card">
             <img
-              src="https://mdbcdn.b-cdn.net/img/new/standard/nature/182.webp"
+              data-src="https://mdbcdn.b-cdn.net/img/new/standard/nature/182.webp"
               class="card-img-top"
               alt="Sunset Over the Sea"
-            />
-            <div class="card-body">
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="card">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/new/standard/nature/182.webp"
-              class="card-img-top"
-              alt="Sunset Over the Sea"
-            />
-            <div class="card-body">
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="card">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/new/standard/nature/182.webp"
-              class="card-img-top"
-              alt="Sunset Over the Sea"
+              v-lazy-load
             />
             <div class="card-body">
               <p class="card-text">
