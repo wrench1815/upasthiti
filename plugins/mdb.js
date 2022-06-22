afc11d44496495d621d241a941c153cf6a532114
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-06-23 00:36:28 +0530

    chore: include overides css in plugin

--- plugins/mdb.js
+++ plugins/mdb.js
@@ -4,6 +4,7 @@ import * as mdb from 'mdb-ui-kit'
 import '~/assets/scss/style-kit.scss'
 
 import '~/assets/css/styles.css'
+import '~/assets/css/overides.css'
 import '~/assets/css/vselect.css'
 
 export default ({ app }, inject) => {
