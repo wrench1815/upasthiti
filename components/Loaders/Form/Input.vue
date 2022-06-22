5fcb770398be3a9f106d1b392276d7dcd080ea20
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-06-22 18:31:02 +0530

    feat: add placeholder for input

---
+++ components/Loaders/Form/Input.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row placeholder-wave">
+    <div class="col-12 col-md-4 mb-2">
+      <span class="placeholder placeholder-sm w-100"></span>
+      <span class="placeholder placeholder-sm w-75"></span>
+    </div>
+    <div class="col">
+      <div class="mb-4">
+        <span class="placeholder placeholder-sm w-100"></span>
+        <span class="placeholder placeholder-sm w-75"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadersFormInput',
+}
+</script>
+
+<style></style>
