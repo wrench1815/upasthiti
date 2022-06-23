0cf6f2172bf3b0e73857d1b9e7dbd0e2a95e167d
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-06-23 19:09:18 +0530

    feat: add role badges

--- components/layout/Dash/User/Table.vue
+++ components/layout/Dash/User/Table.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="table-responsive">
-    <table
-      class="table table-hover mb-0 bg-white align-middle table-borderless"
-    >
+    <table class="table table-hove mb-0 bg-white align-middle table-borderless">
       <thead class="align-middle bg-primary text-white">
         <tr>
-          <th scope="col" class="fw-bolder">NAME</th>
-          <th scope="col" class="fw-bolder">EMAIL</th>
-          <th scope="col" class="fw-bolder">ROLE</th>
-          <th scope="col" class="fw-bolder">DATE ACTIONS</th>
-          <th scope="col" class="fw-bolder">ACTIONS</th>
+          <th scope="col" class="fw-bolder text-uppercase">Name</th>
+          <th scope="col" class="fw-bolder text-uppercase">Email</th>
+          <th scope="col" class="fw-bolder text-uppercase">Role</th>
+          <th scope="col" class="fw-bolder text-uppercase text-nowrap">
+            Date Added
+          </th>
+          <th scope="col" class="fw-bolder text-uppercase">Actions</th>
         </tr>
       </thead>
       <tbody v-if="!loading">
@@ -31,16 +31,77 @@
             </div>
           </td>
           <td>
-            <p class="text-dark mb-0">
+            <div class="text-dark mb-0">
               {{ user.email ? user.email : '----' }}
-            </p>
+            </div>
           </td>
 
           <td>
-            <p class="text-dark mb-0">
-              :v-html="uRole"
-              {{ userRole(user) }}
-            </p>
+            <div class="text-dark mb-0 badge-fs">
+              <!-- if Admin -->
+              <div
+                class="d-flex justify-content-center align-items-center"
+                v-if="userRole(user) === 'Admin'"
+              >
+                <div
+                  class="d-flex justify-content-center align-items-center gap-2 badge bg-gradient-danger badge-fs shadow-3-strong py-2 rounded-pill flex-wrap"
+                >
+                  <i class="ri-admin-fill"></i>
+                  <div class="">Admin</div>
+                </div>
+              </div>
+
+              <!-- if Principal -->
+              <div
+                class="d-flex justify-content-center align-items-center"
+                v-else-if="userRole(user) === 'Principal'"
+              >
+                <div
+                  class="d-flex justify-content-center align-items-center gap-2 badge bg-gradient-danger badge-fs shadow-3-strong py-2 rounded-pill"
+                >
+                  <i class="ri-admin-fill"></i>
+                  <div class="">Principal</div>
+                </div>
+              </div>
+
+              <!-- if HOD -->
+              <div
+                class="d-flex justify-content-center align-items-center"
+                v-else-if="userRole(user) === 'HOD'"
+              >
+                <div
+                  class="d-flex justify-content-center align-items-center gap-2 badge bg-gradient-danger badge-fs shadow-3-strong py-2 rounded-pill"
+                >
+                  <i class="ri-admin-fill"></i>
+                  <div class="">HOD</div>
+                </div>
+              </div>
+
+              <!-- if Teacher -->
+              <div
+                class="d-flex justify-content-center align-items-center"
+                v-else-if="userRole(user) === 'Teacher'"
+              >
+                <div
+                  class="d-flex justify-content-center align-items-center gap-2 badge bg-gradient-danger badge-fs shadow-3-strong py-2 rounded-pill"
+                >
+                  <i class="ri-admin-fill"></i>
+                  <div class="">Teacher</div>
+                </div>
+              </div>
+
+              <!-- fallback -->
+              <div
+                class="d-flex justify-content-center align-items-center"
+                v-else
+              >
+                <div
+                  class="d-flex justify-content-center align-items-center gap-2 badge bg-gradient-danger badge-fs shadow-3-strong py-2 rounded-pill"
+                >
+                  <div class="">----</div>
+                </div>
+              </div>
+            </div>
             <!-- <p class="text-dark mb-0" :v-html="{{userRole(user)}}"></p> -->
           </td>
           <td>
@@ -103,8 +164,7 @@ export default {
     userRole(user) {
       // return Admin if is_admin is true, return teacher if is_teacher is true, return Hod if is_hod is true, return Principal if is_principal is true
       if (user.is_admin) {
-        // return ' Admin'
-        this.uRole = `<span><i class="ri-admin-fill"></i> Admin</span>`
+        return 'Admin'
       } else if (user.is_teacher) {
         return 'Teacher'
       } else if (user.is_hod) {
@@ -125,11 +185,11 @@ export default {
 
 <style scoped>
 .col-hov:hover {
-  transform: scale(1.05);
+  /* transform: scale(1.05);
   box-shadow: 3.8px 4.5px 3.6px rgba(0, 0, 0, 0.024),
     10.5px 12.5px 10px rgba(0, 0, 0, 0.035),
     25.3px 30.1px 24.1px rgba(0, 0, 0, 0.046),
-    84px 100px 80px rgba(0, 0, 0, 0.07);
+    84px 100px 80px rgba(0, 0, 0, 0.07); */
 
   /* border: 0.125rem solid var(--mdb-primary) !important;
   transition: all 0.5s ease-in-out;
@@ -139,4 +199,16 @@ export default {
   border: 0.125rem solid var(transparent) !important;
   transition: all 0.5s ease-in-out;
 } */
+
+.badge-fs {
+  font-size: 0.9rem !important;
+}
+
+table th:first-child {
+  border-radius: 10px 0 0 10px !important;
+}
+
+table th:last-child {
+  border-radius: 0 10px 10px 0;
+}
 </style>
