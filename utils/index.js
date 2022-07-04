281c677e36542a97afd725b3a3544d5614fd428f
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-07-04 13:32:23 +0530

    feat: add util function that returns bootstrap breakpoint

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,26 @@
 export default () => ({
   // return strf time
   dateFormat: (date) => $nuxt.$moment(date).format('Do MMMM YYYY, h:mm:ss a'),
+
+  // returns Bootstrap breakpoint
+  getBsBp: () => {
+    var w =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth
+
+    if (w >= 1400) {
+      return 'xxl'
+    } else if (w >= 1200) {
+      return 'xl'
+    } else if (w >= 992) {
+      return 'lg'
+    } else if (w >= 768) {
+      return 'md'
+    } else if (w >= 576) {
+      return 'sm'
+    } else {
+      return 'xs'
+    }
+  },
 })
