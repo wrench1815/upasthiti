615271a4b4ca6105fb91b68a819b2a3fc7cc5999
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-07-10 16:36:13 +0530

    feat: add pagination component

--- components/Utils/Pagination.vue
+++ components/Utils/Pagination.vue
@@ -1,50 +1,106 @@
 <template>
-  <nav aria-label="...">
-    <ul class="pagination pagination-circle">
-      <!-- start:first -->
-      <li class="page-item">
-        <a class="page-link text-primary">
-          <i class="ri-rewind-fill"></i>
-        </a>
-      </li>
-      <!-- end:first -->
-
-      <!-- start:previous -->
-      <li class="page-item">
-        <a class="page-link">
+  <nav
+    class="d-flex gap-2 align-items-center flex-wrap justify-content-center justify-content-md-star"
+  >
+    <!-- start:previous buttons -->
+    <ul class="d-flex gap-2 list-unstyled mb-0">
+      <li
+        class="rounded-circle border border-1 border-primary hover-item hover-shadow ripple"
+        data-mdb-ripple-color="light"
+        @click="prevPage"
+      >
+        <span class="d-flex justify-content-center align-items-center p-1 px-2">
           <i class="ri-arrow-left-s-line"></i>
-        </a>
+        </span>
       </li>
-      <!-- end:previous -->
+    </ul>
+    <!-- end:previous buttons -->
 
-      <li class="page-item"><a class="page-link">1</a></li>
-      <li class="page-item active" aria-current="page">
-        <a class="page-link"
-          >2 <span class="visually-hidden">(current)</span></a
+    <!-- start:paging -->
+    <ul class="d-flex gap-2 list-unstyled mb-0">
+      <li
+        class="d-flex flex-row rounded-5 bg-light borde border- border-primar text-primar fw-bol"
+      >
+        <span
+          class="d-flex justify-content-center align-items-center p-1 px-2"
+          >{{ this.pagination.current }}</span
+        >
+        <span class="d-flex justify-content-center align-items-center p-1 px-2"
+          >of</span
+        >
+        <span
+          class="d-flex justify-content-center align-items-center p-1 px-2"
+          >{{ this.pagination.count }}</span
         >
       </li>
-      <li class="page-item"><a class="page-link">3</a></li>
-      <!-- start:next -->
-      <li class="page-item">
-        <a class="page-link"><i class="ri-arrow-right-s-line"></i></a>
-      </li>
-      <!-- end:next -->
-
-      <!-- start:last -->
-      <li class="page-item">
-        <NuxtLink to="/dash/user?page=10" class="page-link text-primary">
-          <i class="ri-speed-fill"></i>
-        </NuxtLink>
-      </li>
-      <!-- end:last -->
     </ul>
+    <!-- end:paging -->
+
+    <!-- start:next buttons -->
+    <ul class="d-flex gap-2 list-unstyled mb-0">
+      <li
+        class="rounded-circle border border-1 border-primary hover-item hover-shadow ripple"
+        data-mdb-ripple-color="light"
+        @click="nextPage"
+      >
+        <span class="d-flex justify-content-center align-items-center p-1 px-2">
+          <i class="ri-arrow-right-s-line"></i>
+        </span>
+      </li>
+    </ul>
+    <!-- end:next buttons -->
   </nav>
 </template>
 
 <script>
 export default {
   name: 'UtilsPagination',
+
+  props: {
+    pagination: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  methods: {
+    scrollToTop() {
+      setTimeout(() => {
+        window.scroll({ top: 0, behavior: 'smooth' })
+      }, 100)
+    },
+
+    // previous page
+    prevPage() {
+      if (this.pagination.current > 1) {
+        this.scrollToTop()
+        this.$emit('prevPage', this.pagination.current - 1)
+      }
+    },
+
+    // next page
+    nextPage() {
+      if (this.pagination.current < this.pagination.count) {
+        this.scrollToTop()
+        this.$emit('nextPage', this.pagination.current + 1)
+      }
+    },
+  },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.hover-item {
+  background-color: var(--mdb-white) !important;
+  color: var(--mdb-secondary) !important;
+  border-color: var(--mdb-secondary) !important;
+  height: 2.2rem;
+  width: 2.2rem;
+}
+
+.hover-item:hover {
+  background-color: var(--mdb-primary) !important;
+  color: var(--mdb-white) !important;
+  border-color: var(--mdb-primary) !important;
+}
+</style>
