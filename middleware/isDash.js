a05b0616d1138526667ee5bd15de62a876433536
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-07-15 15:24:01 +0530

    refactor: update isDash middleware logic

--- middleware/isDash.js
+++ middleware/isDash.js
@@ -1,7 +1,10 @@
-// return true fullPath includes '/dash'
-export default function ({ route }) {
-  if (route.fullPath.includes('/dash')) {
+// set isDash true if fullPath starts with /dash
+// return true if fullPath starts with '/dash'
+export default function (context) {
+  if (context.route.fullPath.startsWith('/dash')) {
+    context.store.commit('setDash', true)
     return true
   }
+  context.store.commit('setDash', false)
   return false
 }
