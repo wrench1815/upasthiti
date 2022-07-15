84e05649845b630f446df1251f93ff861d147e4b
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-07-15 13:16:05 +0530

    refactor: update error page styles

--- pages/error_pages/error404.vue
+++ pages/error_pages/error404.vue
@@ -2,24 +2,26 @@
   <section>
     <div class="container py-5 my-4 card card-body rounded-9">
       <div class="row d-flex align-items-center justify-content-center h-100">
-        <div class="col-xl-6 col-md-6 col-sm-12 col-12">
+        <div class="col-md-6 col-12">
           <img
             data-src="~/assets/svg/error404.svg"
-            class="img-fluid vh-65"
-            alt="Error 403"
+            class="img-fluid"
+            alt="Error 404"
             v-lazy-load
           />
         </div>
         <div
-          class="col-xl-6 col-md-6 col-sm-12 col-12 my-2 d-flex flex-column justify-content-center align-items-xl-start align-items-md-start align-items-sm-center align-items-center"
+          class="col-md-6 col-12 my-md-2 my-4 d-flex flex-column justify-content-center align-items-md-start align-items-center"
         >
-          <h2 class="text-uppercase fw-fw-bold text-black">Oh no! error 404</h2>
-          <h3 class="text-black">Page not found</h3>
+          <h2 class="text-uppercase fw-bold text-danger">Oh no! error 404</h2>
+          <h3 class="text-primary">Page not found</h3>
           <span class="fs-6 mt-1 text-center text-md-start"
             >The page you are trying to access doesn't exist or has been
             removed.</span
           >
-          <span class="fs-6 mt-1 text-center text-md-start">Try going back to our homepage.</span>
+          <span class="fs-6 mt-1 text-center text-md-start"
+            >Try going back to our homepage.</span
+          >
           <NuxtLink
             class="btn btn-primary btn-rounded fw-bold text-uppercase my-3"
             to="/"
@@ -30,45 +32,12 @@
       </div>
     </div>
   </section>
-  <!-- <div class="container my-4 p-3">
-    <div class="row">
-      <div class="col-xl-6 col-md-6 col-sm-12 col-12">
-        <img
-          src="~assets/images/error404.png"
-          alt="Error 404 Illustration"
-          class="img-fluid"
-          style=""
-        />
-      </div>
-      <div
-        class="col-xl-6 col-md-6 col-sm-12 col-12 my-2 d-flex flex-column justify-content-center align-items-xl-start align-items-md-start align-items-sm-center align-items-center"
-      >
-        <h2 class="text-uppercase fw-fw-bold text-black">Oh no! error 404</h2>
-        <h3 class="text-black">Page not found</h3>
-        <span class="fs-6 mt-1"
-          >The page you are trying to access doesn't exist or has been
-          removed.</span
-        >
-        <span class="fs-6 mt-1">Try going back to our homepage.</span>
-        <NuxtLink
-          class="btn btn-primary btn-rounded fw-bold text-uppercase my-3"
-          to="/"
-        >
-          Go to homepage
-        </NuxtLink>
-      </div>
-    </div>
-  </div> -->
 </template>
 
 <script>
 export default {
-  name: 'error404',
+  name: 'Error404',
 }
 </script>
 
-<style scoped>
-.vh-65{
-  height: 65vh;
-}
-</style>
+<style></style>
