bd991b4ed981c9df7be2b6ec5e5bec71345c3393
wrench1815 <hardeepkumar1815@gmail.com>
2022-08-08 20:52:28 +0530

    feat: implements - university based filter - paginated university filter - filter chaining university, district and page

--- pages/dash/college/index.vue
+++ pages/dash/college/index.vue
@@ -10,6 +10,7 @@
       <div class="card-body">
         <section
           class="d-flex align-items-center justify-content-center flex-wrap"
+          v-if="!loading"
         >
           <!-- start:Search -->
           <div class="me-auto mb-3">
@@ -21,8 +22,67 @@
           </div>
           <!-- end:Search -->
 
+          <!-- start:University Filter -->
+          <div class="uni-filter-width mb-3 me-0 ms-auto">
+            <v-select
+              placeholder="Select Filter"
+              :options="uniList.results"
+              v-model="selectedUni"
+              :clearable="false"
+              label="alias"
+              @option:selected="filterCollege"
+            >
+              <!-- options -->
+              <template #option="{ alias }">
+                <div
+                  class="d-flex justify-content-start align-items-center gap-1 fw-5 hover-select"
+                >
+                  <span>{{ alias }}</span>
+                </div>
+              </template>
+
+              <!-- selected -->
+              <template #selected-option="{ alias }">
+                <div
+                  class="d-flex justify-content-start align-items-center gap-1"
+                >
+                  <i class="text-primary text-gradient ri-bank-line mt-n1"></i>
+                  <span>{{ alias }}</span>
+                </div>
+              </template>
+
+              <!-- footer for pagination -->
+              <li
+                slot="list-footer"
+                class="d-flex gap-2 justify-content-center align-items-center my-2"
+                v-if="uniList.pagination.count > 1"
+              >
+                <!-- previous button -->
+                <button
+                  class="btn btn-sm btn-floating border border-primary btn-rounded text-primary ripple d-flex justify-content-center align-items-center"
+                  :disabled="disableUniBtns || !uniList.pagination.previous"
+                  @click="uniPaginatePrev"
+                  data-mdb-ripple-color="primary"
+                >
+                  <i class="ri-arrow-left-s-line"></i>
+                </button>
+
+                <!-- next button -->
+                <button
+                  class="btn btn-sm btn-floating border border-primary btn-rounded text-primary ripple d-flex justify-content-center align-items-center"
+                  :disabled="disableUniBtns || !uniList.pagination.next"
+                  @click="uniPaginateNext"
+                  data-mdb-ripple-color="primary"
+                >
+                  <i class="ri-arrow-right-s-line"></i>
+                </button>
+              </li>
+            </v-select>
+          </div>
+          <!-- end:University Filter -->
+
           <!-- start:District Filter -->
-          <div class="filter-width mb-3">
+          <div class="filter-width mb-3 me-0 ms-sm-0 ms-auto">
             <v-select
               placeholder="Select Filter"
               :options="districtsList"
@@ -93,6 +153,10 @@ export default {
       selectedDistrict: 'All',
       districtsList: [],
       payload: {},
+      uniList: {},
+      uniPayload: {},
+      disableUniBtns: true,
+      selectedUni: { alias: 'All' },
     }
   },
 
@@ -115,10 +179,14 @@ export default {
     // on filter
     filterCollege() {
       this.payload = {}
+
+      this.payload.university = this.selectedUni.id
       this.payload.district = this.selectedDistrict
+
       this.$router.push({
         path: this.$route.path,
       })
+
       this.refreshCollege()
     },
 
@@ -136,18 +204,24 @@ export default {
 
       if (this.payload.district) {
         if (this.payload.district == 'All') {
-          this.payload.district = ''
+          this.payload.district = undefined
+        }
+      }
+
+      if (this.payload.university) {
+        if (this.payload.university == 'All') {
+          this.payload.university = undefined
         }
       }
 
       return this.$api.college
-        .list()
+        .list(this.payload)
         .then((response) => {
           this.colleges = response.data
 
           this.error = false
         })
-        .catch((error) => {
+        .catch((err) => {
           if (err.response.status == 404) {
             this.error = true
             this.colleges = []
@@ -203,11 +277,47 @@ export default {
     async setDefaults() {
       this.payload.page = await this.$route.query.page
     },
+
+    // fetch University data for filter
+    async getUni() {
+      this.disableUniBtns = true
+
+      return this.$api.university.list(this.uniPayload).then((response) => {
+        this.uniList = response.data
+
+        // append a new option for All at start
+        this.uniList.results.unshift({
+          id: 'All',
+          alias: 'All',
+        })
+
+        this.disableUniBtns = false
+      })
+    },
+
+    // on Uni filter next
+    uniPaginateNext() {
+      if (this.uniList.pagination.next) {
+        this.uniPayload.page = this.uniPayload.page + 1
+        this.getUni()
+      }
+    },
+
+    // on Uni filter previous
+    uniPaginatePrev() {
+      if (this.uniList.pagination.previous) {
+        this.uniPayload.page = this.uniPayload.page - 1
+        this.getUni()
+      }
+    },
   },
 
   mounted() {
     this.payload.page = this.$route.query.page
     this.districtsList = ['All', ...this.districts]
+
+    this.uniPayload.page = 1
+    this.getUni()
 
     this.getColleges().finally(() => {
       if (!this.error) {
@@ -221,5 +331,9 @@ export default {
 <style scoped>
 .filter-width {
   width: 10.5rem;
+}
+
+.uni-filter-width {
+  width: 11.5rem;
 }
 </style>
