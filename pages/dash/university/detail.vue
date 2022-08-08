023d9a90a09a3cc1e6f5cb8cba6c69f8e0a0b4ac
Ridhima10 <ridhimajain862@gmail.com>
2022-08-08 23:02:21 +0530

    fetch university data and some modifications in layout and data

--- pages/dash/university/detail.vue
+++ pages/dash/university/detail.vue
@@ -5,35 +5,47 @@
       <div class="card-header">
         <h2 class="text-primary text-gradient">University Details</h2>
       </div>
+
+      <div class="card-body" v-if="loading">Loading...</div>
+
       <!-- start:University Info -->
-      <div class="card-body">
+      <div class="card-body" v-else>
         <!-- start:University Info Card -->
         <div
           class="mb-3 shadow-5 d-flex card-header gap-3 rounded-5 align-items-center flex-column flex-md-row"
         >
           <div>
             <img
-              v-lazy-load
               class="avatar avatar-xxl obj-fit-cover shadow-1-strong"
+              :data-src="university.logo"
+              :alt="`${university.alias}'s logo`"
+              v-lazy-load
             />
           </div>
           <div
             class="d-flex flex-column justify-content-center align-content-start text-break"
           >
-            <div class="fw-bold fs-5 text-primary text-gradient">
-              <!-- {{ college.university_name }} -->
-             University Name
+            <!-- start:University Name -->
+            <div
+              class="fw-bold fs-5 text-primary text-gradient text-capitalize"
+            >
+              {{ university.name }}
             </div>
+            <!-- End:University Name -->
+
+            <!-- start:University Email-->
             <div class="text-muted small d-flex gap-1">
               <i class="ri-mail-fill text-primary text-gradient"></i>
-              <!-- {{ college.university_email }} -->
-             Email
+              {{ university.email }}
             </div>
+            <!-- End:University Email -->
+
+            <!-- start:University Phone Number-->
             <div class="text-muted small d-flex gap-1">
               <i class="ri-phone-fill text-primary text-gradient"></i>
-              <!-- {{ college.university_mobile }} -->
-              Phone number
+              {{ university.phone_number }}
             </div>
+            <!-- End:University Phone Number-->
           </div>
         </div>
         <!-- end:user Info Card -->
@@ -41,45 +53,43 @@
         <!-- start:University Alias -->
         <div class="row mb-3">
           <div class="col-12 col-md-4 mt-lg-4 mt-sm-4 d-flex">
-            <label class="text-muted fw-bold-500">University Alias</label>
+            <label class="text-muted fw-bold-500">Alias</label>
           </div>
           <div class="col d-flex mt-lg-4 mt-sm-4">
             <span class="fw-bold-600 fs-6">
-              <!-- {{
-              college.university_alias_name
-            }} -->
+              {{ university.alias }}
             </span>
           </div>
         </div>
         <!-- end:University Alias -->
 
-        <!-- start:University Principal  -->
+        <!-- start:University Vice Chancellor -->
         <div class="row mb-3">
           <div class="col-12 col-md-4 d-flex">
             <label class="text-muted fw-bold-500">Vice Chancellor</label>
           </div>
           <div class="col d-flex">
             <span class="fw-bold-600 fs-6">
-              <!-- {{ principal.first_name }} {{ principal.last_name }} -->
+              {{ university.vice_chancelor }}
             </span>
           </div>
         </div>
-        <!-- end:University Principal -->
+        <!-- end:University Vice Chancellor -->
 
         <!-- start:University Website -->
         <div class="row mb-3">
           <div class="col-12 col-md-4 d-flex">
-            <label class="text-muted fw-bold-500">University Website</label>
+            <label class="text-muted fw-bold-500"> Website</label>
           </div>
           <div class="col d-flex">
-            <!-- <a
-              :href="college.institute_website"
+            <a
+              :href="university.website"
               target="_blank"
               class="text-gradient text-primary"
-              >
-              {{ college.institute_website }}
+            >
+              {{ university.website }}
               <i class="ri-external-link-fill"></i
-            ></a> -->
+            ></a>
           </div>
         </div>
         <!-- end:University Website -->
@@ -87,33 +97,36 @@
         <!-- start:University Address -->
         <div class="row mb-3">
           <div class="col-12 col-md-4 d-flex">
-            <label class="text-muted fw-bold-500">University Address</label>
+            <label class="text-muted fw-bold-500">Address</label>
           </div>
           <div class="col d-flex">
             <span class="fw-bold-600 fs-6">
-              <!-- {{
-              college.university_address
-            }} -->
+              {{ university.address }}
             </span>
           </div>
         </div>
         <!-- end:University Address -->
+
+        <!-- start:College Count -->
+        <div class="row mb-3">
+          <div class="col-12 col-md-4 d-flex">
+            <label class="text-muted fw-bold-500">College Count</label>
+          </div>
+          <div class="col d-flex">
+            <span class="fw-bold-600 fs-6">
+              {{ university.college_count }}
+            </span>
+          </div>
+        </div>
+        <!-- end:College Count-->
       </div>
       <!-- end:University Info -->
     </div>
-     <!-- start:Colleges -->
-    <div class="card mt-4">
-      <div class="card-header">
-        <h2 class="text-primary text-gradient">Colleges</h2>
-      </div>
-
-      <h1 class="d-flex justify-content-center">To-Do</h1>
-    </div>
-    <!-- end:Colleges  -->
   </section>
 </template>
 
 <script>
+import university from '~/api/university'
 export default {
   name: 'DashUniversityDetail',
   layout: 'dash',
@@ -124,9 +137,50 @@ export default {
       loading: true,
     }
   },
+
+  created() {
+    this.$store.commit('breadCrumbs/addBreadCrumb', [
+      {
+        name: 'University',
+        url: '/dash/university',
+      },
+      {
+        name: 'Detail',
+      },
+    ])
+  },
+
+  methods: {
+    async getUni() {
+      let id = this.$route.query.id
+
+      return this.$api.university
+        .retrieve(id)
+        .then((response) => {
+          this.university = response.data
+        })
+        .catch((err) => {
+          if (err.response.status == 404) {
+            this.$nuxt.error({
+              statusCode: 404,
+              message: 'University does not Exist',
+            })
+          } else {
+            this.$nuxt.error({
+              statusCode: 400,
+              message: 'Something went Wrong',
+            })
+          }
+        })
+    },
+  },
+
+  mounted() {
+    this.getUni().then(() => {
+      this.loading = false
+    })
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
