add6560b85aac21b46cc66511f010e5148bc2bf9
wrench1815 <hardeepkumar1815@gmail.com>
2022-08-11 20:49:27 +0530

    refactor: - update markup - add computed properties

--- components/Dash/College/Detail/HodCard.vue
+++ components/Dash/College/Detail/HodCard.vue
@@ -2,42 +2,49 @@
   <div>
     <div class="card-body">
       <div class="card hover-shadow card-hover border mt-5">
-        <div
-          class="bg-image hover-overlay ripple d-flex justify-content-center mt-n5 z-index-5"
-          data-mdb-ripple-color="light"
-        >
+        <!-- start:Profile Image -->
+        <div class="d-flex justify-content-center">
           <img
-            :data-src="hod.profile_image"
-            :alt="`${hod.short_name}'s profile image`"
-            class="img-fluid avatar avatar-xl shadow"
+            :data-src="profileImage"
+            :alt="`${name}'s profile image`"
+            class="img-fluid avatar avatar-xl mt-n5 bg-white border"
             v-lazy-load
           />
         </div>
-        <div class="card-body">
+        <!-- end:Profile Image -->
+
+        <div class="card-body pt-2">
+          <!-- start:Name -->
           <div class="card-title fw-bold fs-5 text-primary text-gradient">
             <NuxtLink
               :to="`/dash/user/detail?id=${hod.id}`"
               class="fw-bolder mb-1 text-primary"
             >
               <span class="better-underline">
-                {{ hod.full_name }}
+                {{ name }}
               </span>
               <i class="ri-link"></i>
             </NuxtLink>
           </div>
+          <!-- end:Name -->
+
+          <!-- start:Email -->
           <div>
             <label class="text-muted fw-bold-500">Email :</label>
             <span class="fw-bold-600 text-dark fs-6 card-text">
-              {{ hod.email }}
+              {{ email }}
             </span>
           </div>
+          <!-- end:Email -->
 
+          <!-- start:Mobile -->
           <div>
             <label class="text-muted fw-bold-500">Mobile:</label>
             <span class="fw-bold-600 text-dark fs-6">
-              {{ hod.mobile ? hod.mobile : '----' }}
+              {{ mobile }}
             </span>
           </div>
+          <!-- end:Mobile -->
         </div>
       </div>
     </div>
@@ -54,7 +61,39 @@ export default {
       required: true,
     },
   },
+
+  computed: {
+    profileImage() {
+      return this.hod.profile_image
+        ? this.hod.profile_image
+        : this.$config.defaultUserImage
+    },
+
+    name() {
+      return this.hod.full_name ? this.hod.full_name : '----'
+    },
+
+    email() {
+      return this.hod.email ? this.hod.email : '----'
+    },
+
+    mobile() {
+      return this.hod.mobile ? this.hod.mobile : '----'
+    },
+  },
 }
 </script>
 
-<style></style>
+<style scoped>
+.card-hover div > img {
+  box-shadow: none;
+  transition: all 0.3s ease-in-out;
+}
+
+.card-hover:hover div > img {
+  box-shadow: 0 2px 15px -3px rgba(0, 0, 0, 0.16),
+    0 10px 20px -2px rgba(0, 0, 0, 0.1);
+  transform: scale(1.1);
+  transition: all 0.3s ease-in-out;
+}
+</style>
--- components/Dash/College/Detail/PrincipalCard.vue
+++ components/Dash/College/Detail/PrincipalCard.vue
@@ -2,41 +2,53 @@
   <div>
     <div class="card border hover-shadow card-hover mt-4 mb-4">
       <div class="d-flex justify-content-start">
+        <!-- start:Profile Image -->
         <img
-          :data-src="principal.profile_image"
-          :alt="`${principal.short_name}'s profile image`"
+          :data-src="profileImage"
+          :alt="`${name}'s profile image`"
           class="img-fluid avatar avatar-xl ms-4 mt-n4 bg-white"
           v-lazy-load
         />
+        <!-- end:Profile Image -->
 
         <div
           class="d-flex flex-column justify-content-center align-content-start text-break"
         >
+          <!-- start:Name -->
           <div
             class="card-title fw-bold fs-5 text-primary text-gradient pt-3 px-2"
           >
+            <!-- link -->
             <NuxtLink
               :to="`/dash/user/detail?id=${principal.id}`"
               class="fw-bolder mb-1 text-primary"
             >
               <span class="better-underline">
-                {{ principal.full_name }}
+                {{ name }}
               </span>
               <i class="ri-link"></i>
             </NuxtLink>
           </div>
+          <!-- start:Name -->
         </div>
       </div>
+
       <div class="card-body pt-2">
+        <!-- start:Email -->
         <label class="text-muted">Email:</label>
         <span class="text-dark fs-6">
-          {{ principal.email }}
+          {{ email }}
         </span>
+        <!-- end:Email -->
+
         <br />
+
+        <!-- start:Mobile -->
         <label class="text-muted">Mobile:</label>
         <span class="text-dark fs-6">
-          {{ principal.mobile ? principal.mobile : '----' }}
+          {{ mobile }}
         </span>
+        <!-- end:Mobile -->
       </div>
     </div>
   </div>
@@ -50,6 +62,26 @@ export default {
     principal: {
       type: Object,
       required: true,
+    },
+  },
+
+  computed: {
+    profileImage() {
+      return this.principal.profile_image
+        ? this.principal.profile_image
+        : this.$config.defaultUserImage
+    },
+
+    name() {
+      return this.principal.full_name ? this.principal.full_name : '----'
+    },
+
+    email() {
+      return this.principal.email ? this.principal.email : '----'
+    },
+
+    mobile() {
+      return this.principal.mobile ? this.principal.mobile : '----'
     },
   },
 }
