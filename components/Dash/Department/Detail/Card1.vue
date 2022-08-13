ba8ae7ef0c4ae53c9bcbaf84833ec39666956b4d
wrench1815 <hardeepkumar1815@gmail.com>
2022-08-13 14:27:07 +0530

    refactor: - seperate sections for college and department conditionally - update page markup

--- components/Dash/Department/Detail/Card1.vue
+++ components/Dash/Department/Detail/Card1.vue
@@ -4,62 +4,80 @@
       class="mb-3 shadow-3-strong d-flex card-header gap-3 rounded-5 align-items-center flex-column flex-md-row bg-light"
     >
       <div>
+        <!-- college Block -->
+        <div
+          class="obj-fit-cover shadow-1-strong bg-primary dept-block"
+          v-if="isDepartment"
+        ></div>
+
         <!-- start:Logo -->
         <img
+          v-if="isCollege"
           :data-src="collegeLogo"
-          :alt="`${isDepartment ? deptName : collegeName}'s logo`"
+          :alt="`${college.name ? college.name : '----'}'s logo`"
           class="avatar avatar-xxl obj-fit-cover shadow-1-strong bg-white"
           v-lazy-load
         />
         <!-- end:Logo -->
       </div>
 
+      <!-- start:Department Section -->
       <div
         class="d-flex flex-column justify-content-center align-content-start text-break"
+        v-if="isDepartment"
       >
         <div class="fw-bold fs-5 text-primary text-gradient">
-          <!-- start:College Name -->
+          <!-- start:Department Name -->
           <div class="fw-bolder mb-1 text-primary" v-if="isDepartment">
-            {{ deptName }}
+            {{ department.name ? department.name : '----' }}
           </div>
-          <!-- end:College Name -->
+          <!-- end:DepartmentName -->
+        </div>
+      </div>
+      <!-- end:Department Section -->
 
-          <!-- start:University Link -->
+      <!-- start:College Section -->
+      <div
+        class="d-flex flex-column justify-content-center align-content-start text-break"
+        v-if="isCollege"
+      >
+        <div class="fw-bold fs-5 text-primary text-gradient">
+          <!-- start:College Link -->
           <NuxtLink
-            :to="`/dash/college/detail?id=${department.college.id}`"
+            :to="`/dash/college/detail?id=${college.id}`"
             class="fw-bolder mb-1 text-primary"
-            v-else
           >
-            <!-- University Name -->
+            <!-- College Name -->
             <span class="better-underline">
-              {{ collegeName }}
+              {{ college.name ? college.name : '----' }}
             </span>
             <i class="ri-link"></i>
           </NuxtLink>
-          <!-- end:University Link -->
+          <!-- end:College Link -->
         </div>
 
         <!--- start:Email -->
         <div class="text-muted small d-flex gap-1">
           <i class="ri-mail-fill text-primary text-gradient"></i>
-          {{ collegeEmail }}
+          {{ college.email }}
         </div>
         <!--- end:Email -->
 
         <!--- start:Mobile -->
         <div class="text-muted small d-flex gap-1">
           <i class="ri-phone-fill text-primary text-gradient"></i>
-          {{ collegeMobile }}
+          {{ college.mobile }}
         </div>
         <!--- end:Mobile -->
 
         <!--- start:District -->
         <div class="text-muted small d-flex gap-1">
           <i class="ri-pin-distance-fill text-primary text-gradient"></i>
-          {{ collegeDistrict }}
+          {{ college.district }}
         </div>
         <!--- end:District -->
       </div>
+      <!-- end:College Section -->
     </div>
   </div>
 </template>
@@ -69,14 +87,21 @@ export default {
   name: 'DashDepartmentDetailCard1',
 
   props: {
+    // for department
     department: {
       type: Object,
-      required: true,
     },
+
     isDepartment: {
       type: Boolean,
       default: false,
     },
+
+    // for college
+    college: {
+      type: Object,
+    },
+
     isCollege: {
       type: Boolean,
       default: false,
@@ -86,62 +111,19 @@ export default {
   computed: {
     collegeLogo() {
       if (this.isCollege) {
-        return this.department.college.logo
-          ? this.department.college.logo
+        return this.college.logo
+          ? this.college.logo
           : this.$config.defaultCollegeImage
-      }
-    },
-
-    collegeName() {
-      return this.department.college.name
-        ? this.department.college.name
-        : '----'
-    },
-
-    deptName() {
-      return this.department.department_name
-        ? this.department.department_name
-        : '----'
-    },
-    collegeEmail() {
-      // if (this.isCollege) {
-      return this.department.college.email
-        ? this.department.college.email
-        : '----'
-      // }
-    },
-
-    // email() {
-    //   if (this.isDepartment) {
-    //     return this.department.email ? this.department.email : '----'
-    //   } else {
-    //     return this.department.college.email
-    //       ? this.department.college.email
-    //       : '----'
-    //   }
-    // },
-
-    collegeMobile() {
-      if (this.isDepartment) {
-        return this.department.mobile ? this.department.mobile : '----'
-      } else {
-        return this.department.college.mobile
-          ? this.department.college.mobile
-          : '----'
-      }
-    },
-
-    collegeDistrict() {
-      if (this.isDepartment) {
-        return this.department.district ? this.department.district : '----'
-      } else {
-        return this.department.college.district
-          ? this.department.college.district
-          : '----'
       }
     },
   },
 }
 </script>
 
-<style></style>
+<style scoped>
+.dept-block {
+  width: 1.5rem !important;
+  height: 4rem !important;
+  border-radius: 0.75rem 0 0 0.75rem !important;
+}
+</style>
