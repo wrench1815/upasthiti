103bcc2c55948cf88a02c80ee97a6e430d6e62e1
ishagupta2110 <ishagupta1021@gmail.com>
2022-08-13 11:47:39 +0530

    create about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,34 +1,223 @@
 <template>
   <section class="container my-4 card">
-    <div class="d-flex justify-content-center">
-      <div class="row">
-        <div class="col-12 pb-4">
-          <h2>ABOUT US</h2>
-        </div>
-      </div>
-    </div>
-    <div class="row mb-4">
-      <div class="col-md-6 col-lg-6 col-12 mb-2 ps-md-5">
+    <div class="my-4">
+      <div class="text-center my-4">
+        <h2 class="fw-bold text-primary">Our Mission</h2>
+        <p class="mx-auto w-sm-75 w-md-50 mt-3">
+          " Replacing manually mark attendance of students by hand on a register
+          to take Online attendance and maintain attendance data and turn it
+          into analytical data so that various aspects of a student's
+          performance in a class can be analyzed. "
+        </p>
         <img
-          src="~/assets/images/about.jpg"
-          class="img-fluid"
-          alt="attendance confirmed illustration"
+          src="~/assets/svg/aboutpage.svg"
+          class="img-fluid p-sm-2 p-md-2 p-xl-5"
+          alt="aboutpage image"
         />
       </div>
-      <div
-        class="col-md-6 col-lg-6 col-12 d-flex justify-content-center align-items-center"
-      >
-        <p class="fs-5">
-          Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
-          consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Harum obcaecati consequuntur consectetur autem eius explicabo dolore
-          numquam ducimus quae! Et, earum atque! Lorem, ipsum dolor sit amet
-          consectetur adipisicing elit. Deserunt officia numquam expedita
-          corporis! Dolorum veniam, voluptatum deserunt, nam vero repellendus
-          nobis quas eaque ipsum non quibusdam possimus vitae necessitatibus
-          rerum dol Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          Aliquam, rem.
-        </p>
+      <div class="row container my-4">
+        <div class="col-md-4 col-12">
+          <h3
+            class="text-primary fw-bold d-flex justify-content-center align-items-center h-100"
+          >
+            <span class="vertical-text text-uppercase text-shadow">What?</span>
+          </h3>
+        </div>
+        <div class="col">
+          <div class="mt-3 text-center text-md-start">
+            <p>
+              Upasthiti is an Attendance Management and Analytics Web
+              Application that will come with a Companion Android App used for
+              taking Attendance. This project aims at solving the problem of
+              maintaining and managing Attendance of Students. And generate
+              useful data insights out of it. This will benefit not only
+              Teachers and Students but also the Institute Administration, State
+              Level Administration and Parents as well.
+            </p>
+            <p>
+              This will help maintain Data of Students such as Institute
+              Affiliation, Geographical Migration and other Insights like Ratio
+              of Classes Attended, Monthly/Weekly/Session Attendance Data.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row container my-3">
+        <div class="col-md-4 col-12">
+          <h3
+            class="text-primary fw-bold d-flex justify-content-center align-items-center h-100"
+          >
+            <span class="vertical-text text-uppercase text-shadow">Why?</span>
+          </h3>
+        </div>
+        <div class="col">
+          <div class="mt-3 text-center text-md-start">
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
+              consectetur. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Harum obcaecati consequuntur consectetur autem eius
+              explicabo dolore numquam ducimus quae! Et, earum atque! Lorem,
+              ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+              officia numquam expedita corporis!
+            </p>
+            <p>
+              Dolorum veniam, voluptatum deserunt, nam vero repellendus nobis
+              quas eaque ipsum non quibusdam possimus vitae necessitatibus rerum
+              dol Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Aliquam, rem.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
+              consectetur. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Harum obcaecati consequuntur consectetur autem eius
+              explicabo dolore numquam ducimus quae! Et, earum atque! Lorem,
+              ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+              officia numquam expedita .
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="my-4">
+        <h3 class="text-center text-primary fw-bold">Meet our Developer</h3>
+        <div class="row mt-4 gy-3">
+          <div class="col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-12">
+            <div class="card p-3">
+              <div class="row">
+                <div class="col-lg-4 col-md-5 col-sm-4 col-12  text-center text-sm-start">
+                  <img
+                    class="avatar avatar-xxl obj-fit-cover shadow-1-strong"
+                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330"
+                    alt=""
+                    v-lazy-load
+                  />
+                </div>
+                <div class="col-lg-8  col-md-7 col-sm-8  col-12 text-center text-sm-start ">
+                  <div
+                    class="fw-bold fs-5 text-primary text-gradient text-capitalize"
+                  >
+                    Ridhima Jain
+                  </div>
+                  <div class="fs-6 text-secondary">Frontend Developer</div>
+
+                  <div class="text-muted small d-flex gap-1 justify-content-center justify-content-sm-start">
+                    <i class="ri-mail-fill text-primary text-gradient"></i>
+                    <a href="mailto:ridhimajain862@gmail.com">
+                      <span class="text-dark text-break">
+                        ridhimajain862@gmail.com</span
+                      >
+                    </a>
+                  </div>
+                  <div class="text-muted small d-flex gap-1 justify-content-center justify-content-sm-start" >
+                    <i class="ri-github-fill text-primary text-gradient"></i>
+                    <a href="https://github.com/Ridhima10">
+                      <span class="text-dark"> Ridhima Jain</span>
+                    </a>
+                  </div>
+                  <div class="text-muted small d-flex gap-1 justify-content-center justify-content-sm-start">
+                    <i
+                      class="ri-linkedin-box-fill text-primary text-gradient"
+                    ></i>
+                    <a
+                      href="https://www.linkedin.com/in/ridhima-jain-96829a212/"
+                    >
+                      <span class="text-dark"> Ridhima Jain</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-12">
+            <div class="card p-3">
+              <div class="row">
+                <div class="col-lg-4 col-md-5 col-sm-4 col-12  text-center text-sm-start">
+                  <img
+                    class="avatar avatar-xxl obj-fit-cover shadow-1-strong"
+                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330"
+                    alt=""
+                    v-lazy-load
+                  />
+                </div>
+                <div class="col-lg-8  col-md-7 col-sm-8  col-12 text-center text-sm-start">
+                  <div
+                    class="fw-bold fs-5 text-primary text-gradient text-capitalize"
+                  >
+                    Hardeep Kumar
+                  </div>
+                  <div class="fs-6 text-secondary">Backend Developer</div>
+
+                  <div class="text-muted small d-flex gap-1 justify-content-center justify-content-sm-start">
+                    <i class="ri-mail-fill text-primary text-gradient"></i>
+                    <a href="mailto:hardeepkumar1815@gmail.com">
+                      <span class="text-dark text-break">
+                        hardeepkumar1815@gmail.com</span
+                      >
+                    </a>
+                  </div>
+                  <div class="text-muted small d-flex gap-1 justify-content-center justify-content-sm-start">
+                    <i class="ri-github-fill text-primary text-gradient"></i>
+                    <a href="https://github.com/wrench1815">
+                      <span class="text-dark"> Hardeep Kumar</span>
+                    </a>
+                  </div>
+                  <div class="text-muted small d-flex gap-1 justify-content-center justify-content-sm-start">
+                    <i
+                      class="ri-linkedin-box-fill text-primary text-gradient"
+                    ></i>
+                    <a href="https://www.linkedin.com/in/hardeep-kumar1815/">
+                      <span class="text-dark"> Hardeep Kumar</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-12 mx-auto">
+            <div class="card p-3">
+              <div class="row">
+                <div class="col-lg-4 col-md-5 col-sm-4 col-12  text-center text-sm-start">
+                  <img
+                    class="avatar avatar-xxl obj-fit-cover shadow-1-strong"
+                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330"
+                    alt=""
+                    v-lazy-load
+                  />
+                </div>
+                <div class="col-lg-8  col-md-7 col-sm-8  col-12 text-center text-sm-start">
+                  <div
+                    class="fw-bold fs-5 text-primary text-gradient text-capitalize"
+                  >
+                    Isha Gupta
+                  </div>
+                  <div class="fs-6 text-secondary">Frontend Developer</div>
+
+                  <div class="text-muted small d-flex gap-1 justify-content-center justify-content-sm-start">
+                    <i class="ri-mail-fill text-primary text-gradient"></i>
+                    <a href="mailto:ishagupta1021@gmail.com">
+                      <span class="text-dark text-break"
+                        >ishagupta1021@gmail.com</span
+                      >
+                    </a>
+                  </div>
+                  <div class="text-muted small d-flex gap-1 justify-content-center justify-content-sm-start">
+                    <i class="ri-github-fill text-primary text-gradient"></i>
+                    <a href="https://github.com/ishagupta2110">
+                      <span class="text-dark"> Isha Gupta</span>
+                    </a>
+                  </div>
+                  <div class="text-muted small d-flex gap-1 justify-content-center justify-content-sm-start">
+                    <i
+                      class="ri-linkedin-box-fill text-primary text-gradient"
+                    ></i>
+                    <a href="https://www.linkedin.com/in/isha-gupta-188843214/">
+                      <span class="text-dark"> Isha Gupta</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -36,8 +225,19 @@
 
 <script>
 export default {
-  name: 'AboutPage',
+  name: 'About',
 }
 </script>
 
-<style></style>
+<style scoped>
+@media (min-width: 768px) {
+  .vertical-text {
+    writing-mode: vertical-lr;
+    text-orientation: upright;
+  }
+
+  .text-shadow {
+    text-shadow: 4px 4px 2px rgb(0 0 0 / 15%);
+  }
+}
+</style>
