25a650bee6814e5f88f45dc9c25492651e7ce208
ishagupta2110 <ishagupta1021@gmail.com>
2022-08-13 14:31:58 +0530

    create privacy policy page

---
+++ pages/privacy.vue
@@ -0,0 +1,156 @@
+<template>
+  <section class="container my-4 card p-3">
+    <h2 class="text-primary fw-bold text-center my-4">Privacy Policy</h2>
+    <div class="row mb-2">
+      <div class="col-md-6 col-lg-6 col-12 ps-md-5 my-3">
+        <img
+          src="~/assets/svg/policy.svg"
+          class="img-fluid sticky-md-top z-index-1"
+          alt="Privacy Policy illustration"
+        />
+      </div>
+
+      <div
+        class="row col-md-6 col-lg-6 col-12 d-flex justify-content-center p-4 p-md-5"
+      >
+        <p>
+          At <span class="text-info fw-bold">Upasthiti</span>, accessible from
+          <NuxtLink to="/" class="link-info fw-bold">Upasthiti</NuxtLink>, one
+          of our main priorities is the privacy of our visitors. This Privacy
+          Policy document contains types of information that is collected and
+          recorded by
+          <NuxtLink to="/" class="link-info fw-bold">Upasthiti</NuxtLink>
+          and how we use it.
+        </p>
+
+        <p>
+          If you have additional questions or require more information about our
+          Privacy Policy, do not hesitate to contact us.
+        </p>
+
+        <p>
+          This Privacy Policy applies only to our online activities and is valid
+          for visitors to our website with regards to the information that they
+          shared and/or collect in
+          <NuxtLink to="/" class="link-info fw-bold">Upasthiti</NuxtLink>. This
+          policy is not applicable to any information collected offline or via
+          channels other than this website.
+        </p>
+
+        <h2 class="">Consent</h2>
+
+        <p>
+          By using our website, you hereby consent to our Privacy Policy and
+          agree to its terms.
+        </p>
+
+        <h2>Information we collect</h2>
+
+        <p>
+          The personal information that you are asked to provide, and the
+          reasons why you are asked to provide it, will be made clear to you at
+          the point we ask you to provide your personal information.
+        </p>
+        <p>
+          If you contact us directly, we may receive additional information
+          about you such as your name, email address, phone number, the contents
+          of the message and/or attachments you may send us, and any other
+          information you may choose to provide.
+        </p>
+        <p>
+          When you register for an Account, we may ask for your contact
+          information, including items such as name, company name, address,
+          email address, and telephone number.
+        </p>
+
+        <h2>How we use your information</h2>
+
+        <p>We use the information we collect in various ways, including to:</p>
+
+        <ul class="ms-5">
+          <li>Provide, operate, and maintain our website</li>
+          <li>Improve, personalize, and expand our website</li>
+          <li>Understand and analyze how you use our website</li>
+          <li>Develop new products, services, features, and functionality</li>
+          <li>
+            Communicate with you, either directly or through one of our
+            partners, including for customer service, to provide you with
+            updates and other information relating to the website, and for
+            marketing and promotional purposes
+          </li>
+          <li>Send you emails</li>
+          <li>Find and prevent fraud</li>
+        </ul>
+
+        <h2>Log Files</h2>
+
+        <p>
+          <span class="text-info fw-bold">Upasthiti</span> follows a standard
+          procedure of using log files. These files log visitors when they visit
+          websites. All hosting companies do this and a part of hosting
+          services' analytics. The information collected by log files include
+          internet protocol (IP) addresses, browser type, Internet Service
+          Provider (ISP), date and time stamp, referring/exit pages, and
+          possibly the number of clicks. These are not linked to any information
+          that is personally identifiable. The purpose of the information is for
+          analyzing trends, administering the site, tracking users' movement on
+          the website, and gathering demographic information.
+        </p>
+
+        <h2>Cookies and Web Beacons</h2>
+
+        <p>
+          Like any other website,
+          <NuxtLink to="/" class="link-info fw-bold">Upasthiti</NuxtLink>
+          uses 'cookies'. These cookies are used to store information including
+          visitors' preferences, and the pages on the website that the visitor
+          accessed or visited. The information is used to optimize the users'
+          experience by customizing our web page content based on visitors'
+          browser type and/or other information.
+        </p>
+
+        <h2>Third Party Privacy Policies</h2>
+
+        <p>
+          <span class="text-info fw-bold">Upasthiti's</span> Privacy Policy does
+          not apply to other advertisers or websites. Thus, we are advising you
+          to consult the respective Privacy Policies of these third-party ad
+          servers for more detailed information. It may include their practices
+          and instructions about how to opt-out of certain options.
+        </p>
+
+        <p>
+          You can choose to disable cookies through your individual browser
+          options. To know more detailed information about cookie management
+          with specific web browsers, it can be found at the browsers'
+          respective websites.
+        </p>
+
+        <h2>Children's Information</h2>
+
+        <p>
+          Another part of our priority is adding protection for children while
+          using the internet. We encourage parents and guardians to observe,
+          participate in, and/or monitor and guide their online activity.
+        </p>
+
+        <p>
+          <span class="text-info fw-bold">Upasthiti</span> does not knowingly
+          collect any Personal Identifiable Information from children under the
+          age of 13. If you think that your child provided this kind of
+          information on our website, we strongly encourage you to contact us
+          immediately and we will do our best efforts to promptly remove such
+          information from our records.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Privacy Policy',
+}
+</script>
+
+<style></style>
