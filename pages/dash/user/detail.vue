0aa2a52989e630b870511a56997d1d11dddf4c0c
wrench1815 <hardeepkumar1815@gmail.com>
2022-08-20 16:10:37 +0530

    feat: add detail page for User

--- pages/dash/user/detail.vue
+++ pages/dash/user/detail.vue
@@ -1,138 +1,132 @@
 <template>
   <section class="container-fluid my-4">
-    <!-- <Lazy-DashUserCard v-if="!loading" :user="user" /> -->
-    <div class="card">
-      <div class="card-header">
-        <h2 class="text-primary text-gradient">User Details</h2>
+    <transition name="scale-in" mode="out-in">
+      <div class="card card-body" v-if="loading">
+        <Lazy-LoadersDetailUserCard />
       </div>
 
-      <div class="card-body" v-if="loading">
-        <div v-for="item in 6" :key="item">
-          <div class="row placeholder-wave">
-            <div class="col-12 col-md-4 mb-2">
-              <span class="placeholder placeholder-sm w-100"></span>
-              <span class="placeholder placeholder-sm w-75"></span>
-            </div>
-            <div class="col">
-              <div class="mb-4">
-                <span class="placeholder placeholder-sm w-100"></span>
-                <span class="placeholder placeholder-sm w-75"></span>
+      <section v-else>
+        <!-- start:User Info -->
+        <div class="card">
+          <div class="card-header">
+            <h2 class="text-primary text-gradient">User Details</h2>
+          </div>
+          <div class="card-body">
+            <!-- start:User Info Card -->
+            <Lazy-DashUserDetailUCard :user="user" />
+            <!-- end:user Info Card -->
+
+            <section>
+              <!-- start:User District -->
+              <div class="row mb-3">
+                <div class="col-12 col-md-4 mt-lg-4 mt-sm-4 d-flex">
+                  <label class="text-muted fw-bold-500">District</label>
+                </div>
+                <div class="col d-flex mt-lg-4 mt-sm-4">
+                  <span class="fw-bold-600 fs-6">{{
+                    user.district ? user.district : '----'
+                  }}</span>
+                </div>
               </div>
-            </div>
+              <!-- end:User District -->
+
+              <!-- start:User Address -->
+              <div class="row mb-3">
+                <div class="col-12 col-md-4 d-flex">
+                  <label class="text-muted fw-bold-500"> Address</label>
+                </div>
+                <div class="col d-flex">
+                  <span class="fw-bold-600 fs-6">{{
+                    user.address ? user.address : '---'
+                  }}</span>
+                </div>
+              </div>
+              <!-- end:User Address -->
+
+              <!-- start:User Gender -->
+              <div class="row mb-3">
+                <div class="col-12 col-md-4 d-flex">
+                  <label class="text-muted fw-bold-500">Gender</label>
+                </div>
+                <div class="col d-flex">
+                  <span class="fw-bold-600 fs-6">
+                    <i
+                      class="text-primary"
+                      :class="genderIcon(user.gender)"
+                    ></i>
+                    {{ user.gender }}</span
+                  >
+                </div>
+              </div>
+              <!-- end:User Gender -->
+            </section>
           </div>
         </div>
-      </div>
+        <!-- end:User Info -->
+      </section>
+    </transition>
 
-      <!-- start:User Info -->
-      <div class="card-body">
-        <!-- start:User Info Card -->
-        <Lazy-DashUserDetailUCard :user="user" />
-        <!-- end:user Info Card -->
-
-        <section>
-          <!-- start:User District -->
-          <div class="row mb-3">
-            <div class="col-12 col-md-4 mt-lg-4 mt-sm-4 d-flex">
-              <label class="text-muted fw-bold-500">District</label>
-            </div>
-            <div class="col d-flex mt-lg-4 mt-sm-4">
-              <span class="fw-bold-600 fs-6">{{ user.district }}</span>
-            </div>
-          </div>
-          <!-- end:User District -->
-
-          <!-- start:User Address -->
-          <div class="row mb-3">
-            <div class="col-12 col-md-4 d-flex">
-              <label class="text-muted fw-bold-500"> Address</label>
-            </div>
-            <div class="col d-flex">
-              <span class="fw-bold-600 fs-6">{{ user.address }}</span>
-            </div>
-          </div>
-          <!-- end:User Address -->
-
-          <!-- start:User Gender -->
-          <div class="row mb-3">
-            <div class="col-12 col-md-4 d-flex">
-              <label class="text-muted fw-bold-500">Gender</label>
-            </div>
-            <div class="col d-flex">
-              <span class="fw-bold-600 fs-6">
-                <i :class="genderIcon"></i> {{ user.gender }}</span
-              >
-            </div>
-          </div>
-          <!-- end:User Gender -->
-        </section>
-      </div>
-      <!-- end:User Info -->
-    </div>
     <!-- start:Principal -->
-    <div class="card mt-4">
-      <div class="card-header">
-        <h2 class="text-primary text-gradient">Principal</h2>
+    <transition name="scale-in" mode="out-in">
+      <div class="card card-body mt-4" v-if="loading">
+        <Lazy-LoadersDetailUserCollegeCard />
       </div>
-      <!-- <DashUserDetailCard /> -->
-      <div class="card-body">
-        <!-- start:Principal Card -->
-        <div class="row">
-          <div
-            class="col-sm-6 col-lg-6 col-md-6 col-12"
-            v-for="item in 1"
-            :key="item"
-          >
-            <Lazy-DashUserDetailPrincipalCard />
+
+      <section v-else>
+        <div class="card mt-4" v-if="user.is_principal">
+          <div class="card-header">
+            <h2 class="text-primary text-gradient">Administered College</h2>
+          </div>
+
+          <div class="card-body">
+            <!-- start:College Card -->
+            <Lazy-DashUserDetailCollegeCard
+              :college="user.administrated_college"
+              v-if="user.administrated_college"
+            />
+            <div v-else>
+              <Lazy-UtilsNoData />
+            </div>
+            <!-- end:College Card -->
           </div>
         </div>
-        <!-- end:Principal Card -->
-        <!-- start:College Card -->
-        <h3 class="text-primary text-gradient mt-5 mb-4">College</h3>
-        <Lazy-DashUserDetailCollegeCard />
-        <!-- end:College Card -->
-      </div>
-    </div>
+      </section>
+    </transition>
     <!-- end:Principal  -->
 
     <!-- start:HOD -->
-    <div class="card mt-4">
-      <div class="card-header">
-        <h2 class="text-primary text-gradient">HOD</h2>
+    <transition name="scale-in" mode="out-in">
+      <div class="card card-body mt-4" v-if="loading">
+        <Lazy-LoadersDetailUserCollegeCard />
       </div>
-      <div class="card-body">
-        <!-- start:HOD info Card -->
-        <div class="row">
-          <div
-            class="col-sm-6 col-lg-6 col-md-6 col-12"
-            v-for="item in 1"
-            :key="item"
-          >
-            <Lazy-DashUserDetailHodCard />
+
+      <section v-else>
+        <div class="card mt-4" v-if="user.is_hod">
+          <div class="card-header">
+            <h2 class="text-primary text-gradient">Affiliated College</h2>
           </div>
-        </div>
-        <!-- end:HOD info Card -->
+          <div class="card-body">
+            <!-- start:College Card -->
+            <template v-for="college in user.college">
+              <Lazy-DashUserDetailCollegeCard
+                :college="college"
+                :key="college.id"
+              />
+            </template>
+            <!-- end:College Card -->
+          </div>
 
-        <!-- start:College Card -->
-        <h3 class="text-primary text-gradient mt-5 mb-4">College</h3>
-        <Lazy-DashUserDetailCollegeCard />
-        <!-- end:College Card -->
-      </div>
-
-      <div class="row mx-2">
-        <div
-          class="col-sm-6 col-lg-4 col-md-6 col-12"
-          v-for="item in 1"
-          :key="item"
-        >
-          <h3 class="text-primary text-gradient mt-5 mb-4">Department</h3>
-          <Lazy-DashUserDetailDeptCard />
+          <h2 class="card-header text-primary text-gradient">Department</h2>
+          <section class="card-body" v-if="!loading">
+            <Lazy-DashUserDetailDept :uId="user.id" />
+          </section>
         </div>
-      </div>
-    </div>
+      </section>
+    </transition>
     <!-- end:HOD -->
 
     <!-- start:Teacher-->
-    <div class="card mt-4">
+    <div class="card mt-4 d-none">
       <div class="card-header">
         <h2 class="text-primary text-gradient">Teacher</h2>
       </div>
@@ -152,7 +146,7 @@
 
         <!-- start:College Card -->
         <h3 class="text-primary text-gradient mt-5 mb-4">College</h3>
-        <Lazy-DashUserDetailCollegeCard />
+        <!-- <Lazy-DashUserDetailCollegeCard /> -->
         <!-- end:College Card -->
 
         <!-- start:Course Card -->
@@ -192,21 +186,6 @@ export default {
     ])
   },
 
-  computed: {
-    genderIcon(gen) {
-      if (gen == 'Male') {
-        // if Male
-        return 'ri-men-fill'
-      } else if (gen == 'Female') {
-        // if Female
-        return 'ri-women-fill'
-      } else {
-        // if unspecified
-        return 'ri-genderless-fill'
-      }
-    },
-  },
-
   methods: {
     // fetch user by id
     async getUser() {
@@ -234,6 +213,19 @@ export default {
             })
           }
         })
+    },
+
+    genderIcon(gen) {
+      if (gen == 'Male') {
+        // if Male
+        return 'ri-men-fill'
+      } else if (gen == 'Female') {
+        // if Female
+        return 'ri-women-fill'
+      } else {
+        // if unspecified
+        return 'ri-genderless-fill'
+      }
     },
   },
 
