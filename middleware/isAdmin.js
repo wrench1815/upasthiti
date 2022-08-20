6edfbf1817f6fe752fed792efe08744f00094fe7
wrench1815 <hardeepkumar1815@gmail.com>
2022-08-20 18:31:11 +0530

    feat: add middlewares for - admin - principal - hod - teacher

---
+++ middleware/isAdmin.js
@@ -0,0 +1,8 @@
+// check if user is admin or not
+// if user is admin allow access
+// if user is not admin redirect to dash if logged in else redirect to main site home
+export default function ({ store, redirect }) {
+  if (store.getters.loggedInUserRole != 'admin') {
+    return store.getters.isAuthenticated ? redirect('/dash') : redirect('/')
+  }
+}
---
+++ middleware/isHod.js
@@ -0,0 +1,8 @@
+// check if user is HOD or not
+// if user is HOD allow access
+// if user is not HOD redirect to dash if logged in else redirect to main site home
+export default function ({ store, redirect }) {
+  if (store.getters.loggedInUserRole != 'hod') {
+    return store.getters.isAuthenticated ? redirect('/dash') : redirect('/')
+  }
+}
