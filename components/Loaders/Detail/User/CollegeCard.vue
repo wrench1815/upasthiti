2ec0c06fee8b58a95184335f12339d64a1e7c9da
wrench1815 <hardeepkumar1815@gmail.com>
2022-08-20 16:05:44 +0530

    feat: add loaders

---
+++ components/Loaders/Detail/User/CollegeCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div
+      class="mb-3 shadow-3-strong d-flex card-header gap-3 rounded-5 align-items-center flex-column flex-md-row bg-light"
+    >
+      <div>
+        <Lazy-LoadersImage />
+      </div>
+
+      <div
+        class="d-flex flex-column justify-content-center align-content-start w-100"
+      >
+        <Lazy-LoadersText size="sm" para />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadersDetailUserCollegeCard',
+}
+</script>
+
+<style></style>
---
+++ components/Loaders/Detail/User/DeptCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="row">
+    <div
+      class="col-sm-6 col-lg-4 col-md-6 col-12"
+      v-for="(dept, index) in count"
+      :key="index"
+    >
+      <div>
+        <div class="card my-4 shadow">
+          <div class="card-header fw-bold fs-5 bg-primary text-white">
+            <Lazy-LoadersText size="xs" :length="12" />
+          </div>
+          <div class="card-body">
+            <div>
+              <Lazy-LoadersText size="xs" para />
+            </div>
+            <Lazy-LoadersButton class="mt-4" rounded />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadersDetailUserDeptCard',
+
+  props: {
+    count: {
+      type: Number,
+      default: 1,
+    },
+  },
+}
+</script>
+
+<style></style>
