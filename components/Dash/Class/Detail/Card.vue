5412de6310a5935381abb1c24fde5e9f492b9597
wrench1815 <hardeepkumar1815@gmail.com>
2022-09-27 11:09:10 +0530

    📦 NEW: add detail page for classes also add components needd to display data for class and a card loder

---
+++ components/Dash/Class/Detail/Card.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div
+      class="mb-3 shadow-3-strong d-flex card-header gap-3 rounded-5 align-items-center flex-column flex-md-row bg-light"
+    >
+      <div>
+        <!-- college Block -->
+        <div class="obj-fit-cover shadow-1-strong bg-primary class-block"></div>
+      </div>
+
+      <div
+        class="d-flex flex-column justify-content-center align-content-start text-break"
+      >
+        <div class="fw-bold fs-5 text-primary text-gradient">
+          <!-- start:Class Name -->
+          <div class="fw-bolder mb-1 text-primary">
+            {{ classs.name ? classs.name : '----' }}
+          </div>
+          <!-- end:Class Name -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashClassDetailCard',
+
+  props: {
+    classs: {
+      type: Object,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.class-block {
+  width: 1.5rem !important;
+  height: 4rem !important;
+  border-radius: 0.75rem 0 0 0.75rem !important;
+}
+</style>
--- pages/dash/class/detail.vue
+++ pages/dash/class/detail.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container-fluid my-4">
-    <!-- start:College Detail -->
+    <!-- start:Class Detail -->
     <div class="card">
       <div class="card-header">
-        <h2 class="text-primary text-gradient">College Details</h2>
+        <h2 class="text-primary text-gradient">Class Details</h2>
       </div>
 
       <!-- Start:Loading -->
@@ -12,174 +12,94 @@
       </div>
       <!-- End:Loading -->
 
-      <!-- start:College Info -->
+      <!-- if not loading -->
       <div class="card-body" v-else>
-        <!-- start:College Info Card -->
-        <Lazy-DashCollegeDetailCard1 :college="college" isCollege />
-        <!-- end:College Info Card -->
+        <!-- start:Class Info Card -->
+        <Lazy-DashClassDetailCard :classs="classs" />
+        <!-- end:Class Info Card -->
 
-        <!-- start:College Alias -->
-        <div class="row mb-3">
-          <div class="col-12 col-md-4 mt-lg-4 mt-sm-4 d-flex">
-            <label class="text-muted fw-bold-500">Alias</label>
+        <div>
+          <!-- Start:Code -->
+          <div class="row mb-3">
+            <div class="col-12 col-md-4 mt-lg-4 mt-sm-4 d-flex">
+              <label class="text-muted">Code</label>
+            </div>
+            <div class="col d-flex mt-lg-4 mt-sm-4">
+              <span class="fw-bold fs-6 text-primary">{{ classs.code }}</span>
+            </div>
           </div>
-          <div class="col d-flex mt-lg-4 mt-sm-4">
-            <span class="fw-bold-600 fs-6">{{ college.alias_name }}</span>
+          <!-- End:Code -->
+
+          <!-- Start:Course -->
+          <div class="row mb-3">
+            <div class="col-12 col-md-4 mt-lg-4 mt-sm-4 d-flex">
+              <label class="text-muted">Course Code</label>
+            </div>
+            <div class="col d-flex mt-lg-4 mt-sm-4">
+              <span class="fw-bold fs-6">{{ classs.course.code }}</span>
+            </div>
           </div>
-        </div>
-        <!-- end:College Alias -->
+          <!-- End:Course -->
 
-        <!-- start:College Website -->
-        <div class="row mb-3">
-          <div class="col-12 col-md-4 d-flex">
-            <label class="text-muted fw-bold-500"> Website</label>
+          <!-- Start:Department -->
+          <div class="row mb-3">
+            <div class="col-12 col-md-4 mt-lg-4 mt-sm-4 d-flex">
+              <label class="text-muted">Department</label>
+            </div>
+            <div class="col d-flex mt-lg-4 mt-sm-4">
+              <span class="fw-bold fs-6">{{ classs.department.name }}</span>
+            </div>
           </div>
-          <div class="col d-flex">
-            <a
-              :href="college.website"
-              target="_blank"
-              class="text-gradient text-primary"
-              >{{ college.website }} <i class="ri-external-link-fill"></i
-            ></a>
+          <!-- End:Department -->
+
+          <!-- Start:Session -->
+          <div class="row mb-3">
+            <div class="col-12 col-md-4 mt-lg-4 mt-sm-4 d-flex">
+              <label class="text-muted">Session</label>
+            </div>
+            <div class="col d-flex mt-lg-4 mt-sm-4">
+              <span class="fw-bold fs-6 text-primary"
+                >{{
+                  classs.session_start
+                    ? $nuxt.$utils.dateFormatNoTimeDay(classs.session_start)
+                    : '----'
+                }}
+                -
+                {{
+                  classs.session_end
+                    ? $nuxt.$utils.dateFormatNoTimeDay(classs.session_end)
+                    : '----'
+                }}</span
+              >
+            </div>
           </div>
-        </div>
-        <!-- end:College Website -->
+          <!-- End:Session -->
 
-        <!-- start:College Address -->
-        <div class="row mb-3">
-          <div class="col-12 col-md-4 d-flex">
-            <label class="text-muted fw-bold-500">Address</label>
+          <!-- Start:Teacher -->
+          <div class="row mb-3">
+            <div class="col-12 col-md-4 mt-lg-4 mt-sm-4 d-flex">
+              <label class="text-muted">Teacher</label>
+            </div>
+            <div class="col d-flex mt-lg-4 mt-sm-4">
+              <span class="fs-6">{{ classs.teacher.full_name }} </span>
+            </div>
           </div>
-          <div class="col d-flex">
-            <span class="fw-bold-600 fs-6">{{ college.address }}</span>
+          <!-- End:Teacher -->
+
+          <!-- Start:College -->
+          <div class="row">
+            <div class="col-12 col-md-4 mt-lg-4 mt-sm-4 d-flex">
+              <label class="text-muted">College</label>
+            </div>
+            <div class="col-12 mt-lg-4 mt-sm-4">
+              <Lazy-DashClassDetailCollegeCard :college="classs.college" />
+            </div>
           </div>
-        </div>
-        <!-- end:College Address -->
-
-        <!-- start:Affiliated University -->
-        <div class="mt-5 mb-4">
-          <h3 class="text-primary text-gradient">Affiliated University</h3>
-        </div>
-
-        <!-- start:Affiliated University  Info Card -->
-        <Lazy-DashCollegeDetailCard1 :college="college" isUniversity />
-        <!-- end:Affiliated University  Info Card -->
-
-        <!-- end:Affiliated University-->
-
-        <!-- start: Principal Info -->
-
-        <div class="mt-5">
-          <h3 class="text-primary text-gradient">Principal</h3>
-        </div>
-        <div class="row">
-          <div class="col-md-6 col-12">
-            <!-- start: Principal Info Card -->
-            <Lazy-DashCollegeDetailPrincipalCard
-              :principal="college.principal"
-            />
-            <!-- end: Principal Info Card -->
-          </div>
-        </div>
-
-        <!-- end: Principal Info -->
-      </div>
-      <!-- end:College Info -->
-    </div>
-    <!-- end:College Detail -->
-
-    <!-- start:Department Info -->
-    <div class="card mt-4">
-      <div class="card-header">
-        <h2 class="text-primary text-gradient">Departments</h2>
-      </div>
-      <!-- start:loading -->
-      <div class="card-body" v-if="loading">
-        <Lazy-LoadersDetailCard />
-      </div>
-      <!-- end: Loading -->
-      <div class="row mx-2" v-else>
-        <div
-          class="col-sm-6 col-lg-4 col-md-6 col-12"
-          v-for="item in 3"
-          :key="item"
-        >
-          <Lazy-DashCollegeDetailCard3 />
+          <!-- End:College -->
         </div>
       </div>
     </div>
-    <!-- end:College Department Info -->
-
-    <!-- start:College HOD Info -->
-    <div class="card mt-4">
-      <div class="card-header">
-        <h2 class="text-primary text-gradient">College HODs</h2>
-      </div>
-      <!-- Start:Loading -->
-      <div class="card-body" v-if="loading">
-        <Lazy-LoadersDetailCard />
-      </div>
-      <!-- End:Loading -->
-      <div class="row mx-2" v-else>
-        <template v-if="college.hod.length == 0">
-          <h2>No HODs</h2>
-        </template>
-        <template v-else>
-          <div
-            class="col-sm-6 col-lg-4 col-md-6 col-12"
-            v-for="item in college.hod"
-            :key="item.id"
-          >
-            <Lazy-DashCollegeDetailHodCard :hod="item" />
-          </div>
-        </template>
-      </div>
-    </div>
-    <!-- end:College HOD Info -->
-
-    <!-- start:College Teachers Info -->
-    <div class="card mt-4">
-      <div class="card-header">
-        <h2 class="text-primary text-gradient">College Teachers</h2>
-      </div>
-      <!-- Start:Loading -->
-      <div class="card-body" v-if="loading">
-        <Lazy-LoadersDetailCard />
-      </div>
-      <!-- End:Loading -->
-      <div class="row mx-2" v-else>
-        <div
-          class="col-sm-6 col-lg-4 col-md-6 col-12"
-          v-for="item in 3"
-          :key="item"
-        >
-          <!-- <DashCollegeDetailHodCard/> -->
-        </div>
-      </div>
-    </div>
-    <!-- end:College Teachers Info -->
-
-    <!-- start:College Students Info -->
-    <div class="card mt-4">
-      <div class="card-header">
-        <h2 class="text-primary text-gradient">College Students</h2>
-      </div>
-      <!-- Start:Loading -->
-      <div class="card-body" v-if="loading">
-        <Lazy-LoadersDetailCard />
-      </div>
-      <!-- End:Loading -->
-      <div class="row mx-2" v-else>
-        <div
-          class="col-sm-6 col-lg-4 col-md-6 col-12"
-          v-for="item in 3"
-          :key="item"
-        >
-          <!-- <DashCollegeDetailHodCard /> -->
-        </div>
-      </div>
-    </div>
-    <!-- end:College Students Info -->
+    <!-- end:Class Detail -->
   </div>
 </template>
 
@@ -193,7 +113,8 @@ export default {
       hod: {},
       loading: true,
       error: true,
-      college: {},
+
+      classs: {},
     }
   },
 
@@ -210,19 +131,19 @@ export default {
   },
 
   methods: {
-    async getCollege() {
+    async getClass() {
       let id = this.$route.query.id
 
-      return this.$api.college
+      return this.$api.class
         .retrieve(id)
         .then((response) => {
-          this.college = response.data
+          this.classs = response.data
         })
         .catch((err) => {
           if (err.response.status == 404) {
             this.$nuxt.error({
               statusCode: 404,
-              message: 'College does not Exist',
+              message: 'Class does not Exist',
             })
           } else {
             this.$nuxt.error({
@@ -235,23 +156,11 @@ export default {
   },
 
   mounted() {
-    this.getCollege().then(() => {
+    this.getClass().then(() => {
       this.loading = false
     })
   },
 }
 </script>
 
-<style scoped>
-.card-hover div > img {
-  box-shadow: none;
-  transition: all 0.3s ease-in-out;
-}
-
-.card-hover:hover div > img {
-  box-shadow: 0 2px 15px -3px rgba(0, 0, 0, 0.16),
-    0 10px 20px -2px rgba(0, 0, 0, 0.1);
-  transform: scale(1.1);
-  transition: all 0.3s ease-in-out;
-}
-</style>
+<style scoped></style>
