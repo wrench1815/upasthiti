af8d082c1a4264121ebf242153a3b3819bcd501e
wrench1815 <hardeepkumar1815@gmail.com>
2022-10-02 19:56:31 +0530

    📦 NEW: add functionality to edit existing Class

--- pages/panel/class/detail/_id.vue
+++ pages/panel/class/detail/_id.vue
@@ -1,6 +1,18 @@
 <template>
   <section class="container-fluid my-4">
-    <div class="card"></div>
+    <div class="card">
+      <div class="card-body">
+        <NuxtLink
+          class="text-wrap d-inline-block"
+          :to="`/panel/class/edit/${id}`"
+        >
+          <span class="d-inline">
+            <span class="better-underline"> Edit Class </span>
+            <span class="link"></span>
+          </span>
+        </NuxtLink>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -8,6 +20,24 @@
 export default {
   name: 'PanelClassDetail',
   layout: 'panel',
+
+  computed: {
+    id() {
+      return this.$route.params.id
+    },
+  },
+
+  created() {
+    this.$store.commit('breadCrumbs/addBreadCrumb', [
+      {
+        name: 'Class',
+        url: '/panel/class',
+      },
+      {
+        name: 'Detail',
+      },
+    ])
+  },
 }
 </script>
 
