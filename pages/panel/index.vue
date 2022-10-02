2bbdd382d109e1f7a6aea2113901a574b479d237
Ridhima10 <ridhimajain862@gmail.com>
2022-10-02 16:31:05 +0530

    Layout of panel

--- pages/panel/index.vue
+++ pages/panel/index.vue
@@ -1,5 +1,52 @@
 <template>
-  <section class="container-fluid my-4"></section>
+  <section class="container-fluid my-4">
+    <section class="card card-body">
+      <div class="container">
+        <div class="row mb-3">
+          <div class="col-sm-12 col-md-4 col-lg-4 col-12 py-3">
+            <div class="card shadow vh-45">
+              <div class="card-body"></div>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4 col-12 py-3">
+            <div class="card shadow vh-45">
+              <div class="card-body"></div>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4 col-12 py-3">
+            <div class="card shadow vh-45">
+              <div class="card-body"></div>
+            </div>
+          </div>
+        </div>
+        <div class="row mb-5">
+          <div class="col-sm-12 col-md-4 col-lg-4 col-12 py-3">
+            <div class="card shadow vh-45">
+              <div class="card-body"></div>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4 col-12 py-3">
+            <div class="card shadow vh-45">
+              <div class="card-body"></div>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4 col-12 py-3">
+            <div class="card shadow vh-45">
+              <div class="card-body"></div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-12 col-md-4 col-lg-4 col-12 py-3">
+              <div class="card shadow vh-45">
+                <div class="card-body"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  </section>
 </template>
 
 <script>
