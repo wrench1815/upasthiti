7a90cbd54dd0264a7443c4ed795dac977e56aa78
wrench1815 <hardeepkumar1815@gmail.com>
2022-10-02 16:29:37 +0530

    📦 NEW: add panel pages for class index add card component for classes

---
+++ components/Panel/Class/Card.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="card border hover-shadow">
+    <div class="card-body pb-3">
+      <h5 class="card-title text-primary border-bottom pb-1">
+        <NuxtLink
+          class="hover-underline-animation"
+          :to="`/panel/class/detail/${classs.id}`"
+        >
+          {{ classs.name }}
+        </NuxtLink>
+      </h5>
+      <p class="card-text">
+        <span class="fw-bold"> Code: </span> {{ classs.code }}
+      </p>
+      <p class="card-text">
+        <span class="fw-bold"> Department: </span> {{ classs.department.name }}
+      </p>
+      <p class="card-text">
+        <span class="fw-bold"> Course Code: </span> {{ classs.course.code }}
+      </p>
+      <div class="hover-underline-animation link-primary">Mark Attendance</div>
+      <!-- <button class="btn btn-primary btn-rounded">Mark</button> -->
+    </div>
+    <div class="card-footer">
+      <div class="text-muted">
+        student count:
+        {{ classs.student.length }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PanelClasscard',
+
+  props: {
+    classs: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/panel/class/index.vue
@@ -0,0 +1,162 @@
+<template>
+  <section class="container-fluid my-4">
+    <div
+      class="card card-body bg-transparent d-flex justify-content-end pt-0 align-items-end"
+    >
+      <button class="btn btn-rounded bg-gradient-primary text-white">
+        <span class="d-flex align-items-center gap-1">
+          <i class="ri-add-line ri-lg mt-n1"></i>
+          <span>Add Class</span>
+        </span>
+      </button>
+    </div>
+
+    <transition name="scale-in" mode="out-in" appear>
+      <div class="card card-body" v-if="loading.main">
+        <div v-for="item in 1" :key="item">
+          <LoadersText size="xs" para />
+        </div>
+      </div>
+
+      <div class="card" v-else>
+        <div class="card-header">
+          <h1 class="text-gradient text-primary d-inline-block">Class List</h1>
+        </div>
+        <div class="card-body">
+          <div class="row g-3">
+            <div
+              class="col-12 col-md-6 col-lg-4"
+              v-for="item in classes.results"
+              :key="item.id"
+            >
+              <PanelClassCard :classs="item" />
+            </div>
+
+            <div class="col-12">
+              <transition name="fade-x" mode="out-in">
+                <Lazy-UtilsPagination
+                  class="mt-1"
+                  v-if="!loading.main && classes.pagination.items != 0"
+                  :pagination.sync="classes.pagination"
+                  @prevPage="onPaginated"
+                  @nextPage="onPaginated"
+                />
+              </transition>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PanelClassIndex',
+  layout: 'panel',
+
+  watch: {
+    '$route.query'() {
+      this.payloadWatch()
+      this.refreshClasses()
+    },
+  },
+
+  data() {
+    return {
+      loading: {
+        main: true,
+      },
+      error: true,
+
+      classes: {},
+      payload: {},
+    }
+  },
+
+  created() {
+    this.$store.commit('breadCrumbs/addBreadCrumb', [
+      {
+        name: 'Class',
+        url: '/panel/class',
+      },
+    ])
+  },
+
+  methods: {
+    // watcher for route query
+    async payloadWatch() {
+      if (this.$route.query.page) {
+        this.payload.page = await this.$route.query.page
+      } else {
+        this.payload.page = '1'
+      }
+    },
+
+    async getClasses() {
+      this.loading.main = true
+
+      // if (this.payload.university) {
+      //   if (this.payload.university == 'All') {
+      //     this.payload.university = undefined
+      //   }
+      // }
+
+      return this.$api.class
+        .list(this.payload)
+        .then((response) => {
+          this.classes = response.data
+
+          this.error = false
+        })
+        .catch((err) => {
+          if (err.response.status == 404) {
+            this.error = true
+            this.classes = {}
+
+            this.$nuxt.error({
+              statusCode: 404,
+              message: 'Page not Found',
+            })
+          } else {
+            this.$nuxt.error({
+              statusCode: 400,
+              message: 'Something went Wrong',
+            })
+          }
+        })
+    },
+
+    // refresh Classes
+    refreshClasses() {
+      this.getClasses().then(() => {
+        this.loading.main = false
+      })
+    },
+
+    // on paginated
+    onPaginated(pageNum) {
+      this.payload.page = pageNum
+      this.$router.push({
+        path: this.$route.path,
+        query: {
+          page: pageNum,
+        },
+      })
+    },
+  },
+
+  mounted() {
+    this.payload.page = this.$route.query.page
+    this.payload.size = 1
+
+    this.getClasses().then(() => {
+      if (!this.error) {
+        this.loading.main = false
+      }
+    })
+  },
+}
+</script>
+
+<style></style>
